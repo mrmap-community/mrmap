2d39d5c0bed17f48dd319cf179c7e511d379f024
peltriau <michel.peltriaux@vermkv.rlp.de>
2019-09-16 09:20:02 +0200

    API and GetCapabilities activity dependency
    * if a service is disabled, it won't be found by the API in any way
    * the getCapabilities URI will not return the GetCapabilities document
    * in both cases a stauts 423 response will be returned (locked resource)

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from rest_framework import viewsets
@@ -48,14 +49,19 @@ class ServiceViewSet(viewsets.GenericViewSet):
         Returns:
              The queryset
         """
-        self.queryset = Service.objects.all()
+        self.queryset = Service.objects.filter(
+            metadata__is_active=True,
+            is_root=True
+        )
 
         # filter by service or service+layers
         las = self.request.query_params.get("las", False)
-        las = not utils.resolve_boolean_attribute_val(las)
-        self.queryset = self.queryset.filter(
-            is_root=las
-        )
+        las = utils.resolve_boolean_attribute_val(las)
+        if las:
+            self.queryset = self.queryset.filter(
+                Q(is_root=las) |
+                Q(is_root=not las)
+            )
 
         # filter by type
         service_type = self.request.query_params.get("type", None)
@@ -65,7 +71,7 @@ class ServiceViewSet(viewsets.GenericViewSet):
         query = self.request.query_params.get("q", None)
         self.queryset = view_helper.filter_queryset_service_query(self.queryset, query)
 
-        # filter by qorganization
+        # filter by organization
         org = self.request.query_params.get("orgid", None)
         self.queryset = view_helper.filter_queryset_services_organization_id(self.queryset, org)
 
@@ -76,7 +82,7 @@ class ServiceViewSet(viewsets.GenericViewSet):
         return self.queryset
 
     # https://docs.djangoproject.com/en/dev/topics/cache/#the-per-view-cache
-    # Cache requested url for 1 hour
+    # Cache requested url for time t
     @method_decorator(cache_page(API_CACHE_TIME))
     def list(self, request):
         tmp = self.paginate_queryset(self.queryset)
@@ -87,14 +93,18 @@ class ServiceViewSet(viewsets.GenericViewSet):
         pass
 
     # https://docs.djangoproject.com/en/dev/topics/cache/#the-per-view-cache
-    # Cache requested url for 1 hour
+    # Cache requested url for time t
     @method_decorator(cache_page(API_CACHE_TIME))
     def retrieve(self, request, pk=None):
         try:
             tmp = Layer.objects.get(id=pk)
+            if not tmp.metadata.is_active:
+                return Response(status=423)
             serializer = LayerSerializer(tmp)
         except ObjectDoesNotExist:
             tmp = Service.objects.get(id=pk)
+            if not tmp.metadata.is_active:
+                return Response(status=423)
             serializer = ServiceSerializer(tmp)
 
         return Response(serializer.data)
@@ -128,7 +138,9 @@ class LayerViewSet(viewsets.GenericViewSet):
         Returns:
              The queryset
         """
-        self.queryset = Layer.objects.all()
+        self.queryset = Layer.objects.filter(
+            metadata__is_active=True
+        )
 
         # filter by parent id
         pid = self.request.query_params.get("pid", None)
@@ -149,7 +161,7 @@ class LayerViewSet(viewsets.GenericViewSet):
         return self.queryset
 
     # https://docs.djangoproject.com/en/dev/topics/cache/#the-per-view-cache
-    # Cache requested url for 1 hour
+    # Cache requested url for time t
     @method_decorator(cache_page(API_CACHE_TIME))
     def list(self, request):
         tmp = self.paginate_queryset(self.queryset)
@@ -160,10 +172,12 @@ class LayerViewSet(viewsets.GenericViewSet):
         pass
 
     # https://docs.djangoproject.com/en/dev/topics/cache/#the-per-view-cache
-    # Cache requested url for 1 hour
+    # Cache requested url for time t
     @method_decorator(cache_page(API_CACHE_TIME))
     def retrieve(self, request, pk=None):
         tmp = Layer.objects.get(id=pk)
+        if not tmp.metadata.is_active:
+            return Response(status=423)
         return Response(LayerSerializer(tmp).data)
 
     def update(self, request, pk=None):
@@ -221,7 +235,9 @@ class MetadataViewSet(viewsets.GenericViewSet):
         Returns:
              The queryset
         """
-        self.queryset = Metadata.objects.all()
+        self.queryset = Metadata.objects.filter(
+            is_active=True,
+        )
 
         # filter by query
         query = self.request.query_params.get("q", None)
@@ -234,7 +250,7 @@ class MetadataViewSet(viewsets.GenericViewSet):
         return self.queryset
 
     # https://docs.djangoproject.com/en/dev/topics/cache/#the-per-view-cache
-    # Cache requested url for 1 hour
+    # Cache requested url for time t
     @method_decorator(cache_page(API_CACHE_TIME))
     def list(self, request):
         tmp = self.paginate_queryset(self.queryset)
@@ -245,10 +261,12 @@ class MetadataViewSet(viewsets.GenericViewSet):
         pass
 
     # https://docs.djangoproject.com/en/dev/topics/cache/#the-per-view-cache
-    # Cache requested url for 1 hour
+    # Cache requested url for time t
     @method_decorator(cache_page(API_CACHE_TIME))
     def retrieve(self, request, pk=None):
         tmp = Metadata.objects.get(id=pk)
+        if not tmp.is_active:
+            return Response(status=423)
         return Response(MetadataSerializer(tmp).data)
 
     def update(self, request, pk=None):
@@ -287,7 +305,7 @@ class GroupViewSet(viewsets.GenericViewSet):
         return self.queryset
 
     # https://docs.djangoproject.com/en/dev/topics/cache/#the-per-view-cache
-    # Cache requested url for 1 hour
+    # Cache requested url for time t
     @method_decorator(cache_page(API_CACHE_TIME))
     def list(self, request):
         tmp = self.paginate_queryset(self.queryset)
@@ -298,7 +316,7 @@ class GroupViewSet(viewsets.GenericViewSet):
         pass
 
     # https://docs.djangoproject.com/en/dev/topics/cache/#the-per-view-cache
-    # Cache requested url for 1 hour
+    # Cache requested url for time t
     @method_decorator(cache_page(API_CACHE_TIME))
     def retrieve(self, request, pk=None):
         tmp = Group.objects.get(id=pk)
@@ -352,7 +370,9 @@ class CatalogueViewSet(viewsets.GenericViewSet):
         Returns:
              The queryset
         """
-        self.queryset = Metadata.objects.all()
+        self.queryset = Metadata.objects.filter(
+            is_active=True,
+        )
 
         # filter by service type
         type = self.request.query_params.get("type", None)
@@ -365,7 +385,7 @@ class CatalogueViewSet(viewsets.GenericViewSet):
         return self.queryset
 
     # https://docs.djangoproject.com/en/dev/topics/cache/#the-per-view-cache
-    # Cache requested url for 1 hour
+    # Cache requested url for time t
     @method_decorator(cache_page(API_CACHE_TIME))
     def list(self, request):
         tmp = self.paginate_queryset(self.queryset)
@@ -373,8 +393,10 @@ class CatalogueViewSet(viewsets.GenericViewSet):
         return self.get_paginated_response(serializer.data)
 
     # https://docs.djangoproject.com/en/dev/topics/cache/#the-per-view-cache
-    # Cache requested url for 1 hour
+    # Cache requested url for time t
     @method_decorator(cache_page(API_CACHE_TIME))
     def retrieve(self, request, pk=None):
         tmp = Metadata.objects.get(id=pk)
+        if not tmp.is_active:
+            return Response(status=423)
         return Response(CatalogueMetadataSerializer(tmp).data)

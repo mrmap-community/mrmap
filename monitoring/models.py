036c5f2d6db204ee6e5852b7a8ae39e779936165
jansule <j_sule02@uni-muenster.de>
2019-12-09 15:28:25 +0100

    adds monitoring models

--- monitoring/models.py
+++ monitoring/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+from service.models import Metadata
+
+
+class MonitoringSetting(models.Model):
+    metadatas = models.ManyToManyField(Metadata, related_name='monitoring_setting')
+    interval = models.DurationField()
+    timeout = models.IntegerField()
+
+
+class MonitoringResult(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    metadata = models.ForeignKey(Metadata)
+    needs_update = models.BooleanField(null=True, blank=True)
+    error_msg = models.TextField(null=True, blank=True)
+    duration = models.DurationField(null=True, blank=True)
+    diff = models.TextField(null=True, blank=True)

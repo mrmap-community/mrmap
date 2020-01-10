a62170e36f089af365e0d7c492e2cb40abe82075
peltriau <michel.peltriaux@vermkv.rlp.de>
2020-01-10 08:19:41 +0100

    MetadataEnum refactoring
    * refactors service/settings.py

--- service/settings.py
+++ service/settings.py
@@ -13,12 +13,6 @@ from django.contrib.gis.geos import Polygon, GEOSGeometry
 from MapSkinner.settings import BASE_DIR
 from service.helper.enums import ConnectionEnum, VersionEnum
 
-# Metadata types
-MD_TYPE_FEATURETYPE = "featuretype"
-MD_TYPE_DATASET = "dataset"
-MD_TYPE_SERVICE = "service"
-MD_TYPE_LAYER = "layer"
-
 # Some special things
 DEFAULT_CONNECTION_TYPE = ConnectionEnum.REQUESTS
 DEFAULT_SERVICE_VERSION = VersionEnum.V_1_1_1

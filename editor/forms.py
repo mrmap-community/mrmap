5eb54b843c479ead6fe5838d1cc9998bab3bdaea
joki <jonas.kiefer@live.com>
2020-02-17 14:30:03 +0100

    Change Icons to Font Awesome collection
    *

--- editor/forms.py
+++ editor/forms.py
@@ -6,10 +6,13 @@ Created on: 09.07.19
 
 """
 from django.forms import ModelForm, CheckboxInput, CheckboxSelectMultiple
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 from django import forms
+from django.forms.utils import flatatt
 
-from service.models import Metadata
+from service.models import Metadata, Group
 
 
 class MetadataEditorForm(ModelForm):
@@ -52,3 +55,43 @@ class FeatureTypeEditorForm(ModelForm):
             "title",
             "abstract",
         ]
+
+
+class AccessIsRootForm(forms.Form):
+    is_use_proxy = forms.BooleanField(required=False, label="Use proxy", widget=forms.CheckboxInput(), initial=False)
+    is_log_proxy = forms.BooleanField(required=False, label="Log proxy", widget=forms.CheckboxInput(), initial=False)
+
+
+class AccessRestrictAccessForm(forms.Form):
+    is_restricted = forms.BooleanField(required=False, label="Restrict access", widget=forms.CheckboxInput(), initial=False)
+
+
+class AccessGroupForm(forms.Form):
+    groups = forms.ModelMultipleChoiceField(queryset=None, to_field_name='id', widget=CheckboxSelectMultiple())
+
+    def __init__(self, *args, **kwargs):
+        # pop custom kwargs before invoke super constructor and hold them
+        self.groups_queryset = kwargs.pop('groups_queryset')
+
+        # run super constructor to construct the form
+        super(AccessGroupForm, self).__init__(*args, **kwargs)
+
+        # initial the fields with the poped kwargs
+        self.fields['groups'].queryset = self.groups_queryset
+
+
+class AccessGroupGeoJsonForm(forms.Form):
+    group_id = None
+    group_name = None
+    geo_json_geometry = forms.CharField(required=False, widget=forms.Textarea(), disabled=True,)
+
+    def __init__(self, *args, **kwargs):
+        # pop custom kwargs before invoke super constructor and hold them
+        self.geo_json_geometry_id = kwargs.pop('geo_json_geometry_id')
+
+        # run super constructor to construct the form
+        super(AccessGroupGeoJsonForm, self).__init__(*args, **kwargs)
+
+        # initial the fields with the poped kwargs
+        self.fields['geo_json_geometry'].widget.attrs = {'placeholder': 'No spatial restriction for this group.',
+                                                         'id': 'id_geo_json_group_' + str(self.geo_json_geometry_id)}

b111c8ae95a989f5bf7f471cfec23149a88d5698
peltriau <michel.peltriaux@vermkv.rlp.de>
2020-03-09 10:54:31 +0100

    Hotfix
    * adds fix for bug where preview service image rendering using tiff lead to non viewable result on html view
    * adds caching for preview images

--- MapSkinner/cacher.py
+++ MapSkinner/cacher.py
@@ -5,7 +5,11 @@ Contact: michel.peltriaux@vermkv.rlp.de
 Created on: 25.02.20
 
 """
+import json
+
 from django.core.cache import cache
+
+from service.helper.crypto_handler import CryptoHandler
 
 
 class SimpleCacher:
@@ -24,7 +28,7 @@ class SimpleCacher:
         """
         return cache.get("{}{}".format(self.key_prefix, key))
 
-    def set(self, key: str, val: str, use_ttl: bool = True):
+    def set(self, key: str, val, use_ttl: bool = True):
         """ Set a key-value pair.
 
         With use_ttl one can decide whether the record shall be kept all the time or being dropped after time to live
@@ -32,6 +36,7 @@ class SimpleCacher:
 
         Args:
             key (str): The key which is used for finding the results
+            val: The value to be stored
         Returns:
 
         """
@@ -71,3 +76,40 @@ class EPSGCacher(SimpleCacher):
         ttl = ttl or 7 * 24 * 60 * 60  # 7 days
         prefix = "epsg_api_axis_order_"
         super().__init__(ttl, prefix)
+
+class PreviewImageCacher(SimpleCacher):
+    def __init__(self, ttl: int=None, metadata=None):
+        ttl = ttl or 60 * 30  # 30 minutes
+        prefix = "preview_{}_".format(metadata.id)
+        self.crypto_handler = CryptoHandler()
+        super().__init__(ttl, prefix)
+
+    def set(self, params_dict: dict, img: dict, use_ttl: bool=True):
+        """ Special setter for preview images.
+
+        Takes a dict of parameter key-value pairs and an image as bytes and stores the image, using the hashed
+        dict as key
+
+        Args:
+            params_dict (dict): The parameters
+            img (dict): The preview image as bytes in a dict {"response": img(bytes), "content_type": img_mime_type}
+        Returns:
+             nothing
+        """
+        query_data_str = json.dumps(params_dict)
+        _hash = self.crypto_handler.sha256(query_data_str)
+        super().set(key=_hash, val=img, use_ttl=use_ttl)
+
+    def get(self, params_dict: dict):
+        """ Special getter for cached preview images.
+
+        Takes a dict of parameter key-value pairs looks for the cached entry, using the hashed dict as key
+
+        Args:
+            params_dict (dict): The parameters
+        Returns:
+             nothing
+        """
+        query_data_str = json.dumps(params_dict)
+        _hash = self.crypto_handler.sha256(query_data_str)
+        return super().get(_hash)

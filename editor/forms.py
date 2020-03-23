4dbeee2225911052525706e1aee1cb998436617b
peltriau <michel.peltriaux@vermkv.rlp.de>
2020-03-23 13:53:15 +0100

    Fixes mandatory categories bug
    * sets categories field in metadata editor to optional
    * removes unused code

--- editor/forms.py
+++ editor/forms.py
@@ -15,43 +15,13 @@ from service.models import Metadata, Keyword
 class MetadataEditorForm(ModelForm):
     action_url = None
 
-    #keywords = CharField(
-    #    widget=TextInput(
-    #        attrs={
-    #            "autocomplete": "off"
-    #        },
-    #    ),
-    #    help_text=_("Separate keywords from each other using comma.")
-    #)
-
     def __init__(self, *args, **kwargs):
         # first call parent's constructor
         super(MetadataEditorForm, self).__init__(*args, **kwargs)
 
         # there's a `fields` property now
         self.fields['terms_of_use'].required = False
-
-        # Transform list of ids to ',' separated keyword string
-        #if kwargs.get("instance", None) is not None:
-        #    self.initial["keywords"] = ",".join([k.keyword for k in self.instance.keywords.all()])
-
-    #def clean_keywords(self):
-    #    """ Resolve simple string keywords to database obejcts
-#
-    #    Returns:
-    #         kw_records (list): A list of keyword records
-    #    """
-    #    super().clean()
-    #    # Transform keywords into list of ids
-    #    keywords = self.data.get("keywords", []).split(",")
-#
-    #    kw_records = []
-    #    # All existing keyword
-    #    for kw in keywords:
-    #        kw_record = Keyword.objects.get_or_create(keyword=kw)[0]
-    #        kw_records.append(kw_record)
-#
-    #    return kw_records
+        self.fields['categories'].required = False
 
     class Meta:
         model = Metadata
@@ -63,7 +33,7 @@ class MetadataEditorForm(ModelForm):
             "keywords",
             "categories",
         ]
-        help_texts={
+        help_texts = {
             "title": _("Edit the title."),
             "abstract": _("Edit the description. Keep it short and simple."),
             "access_constraints": _("Edit the access constraints."),

b1bb552d4f91d266726669705b1857f488cafabd
hollsandre <andre.holl@vermkv.rlp.de>
2020-04-08 10:59:14 +0200

    minor fix

--- install/mass_register.py
+++ install/mass_register.py
@@ -1,20 +1,45 @@
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+import sys
 
-host="https://192.168.56.111"
+host="http://127.0.0.1"
 user="root"
 pw="root"
 
 #  read wms file
-with open('wmslist') as f:
+with open(sys.argv[1]) as f:
     lines = [line.rstrip() for line in f]
 
-# enable session <3
-session = requests.Session()
-# perform login
-req = session.post(host, data={'username': user, 'next': '', 'password': pw, 'next': '','next': '','next': ''}, verify=False)
+cookies = {
+}
+
+data1 = [
+  ('username', user),
+  ('next', ''),
+  ('next', ''),
+  ('next', ''),
+  ('next', ''),
+  ('password', pw),
+]
+
+#login
+r1 = requests.post(host, cookies=cookies, data=data1, verify=False)
+
+data2 = {
+  'page': '2',
+  'is_form_update': 'False',
+  'ogc_request': 'GetCapabilities',
+  'ogc_service': 'wms',
+  'ogc_version': '1.1.1',
+  'uri': '',
+  'registering_with_group': 1,
+  'registering_for_other_organization': ''
+}
+
 # make register requests
 for url in lines:
-    req = session.post(host+"/service/add", data={'page': 2, 'is_form_update': False, 'ogc_request': 'GetCapabilities', 'ogc_service': 'wms','ogc_version': '1.1.1','registering_with_group': 1,'registering_for_other_organization': '','uri': url}, verify=False)
+    data2['uri'] = url
+    r2 = requests.post(host+'/service/add/', cookies=r1.cookies, data=data2, verify=False)
+    #req = session.post(host+"/service/add", data={'REQUEST': 'GetCapabilities' ,'SERVICE': 'WMS','page': 2, 'is_form_update': False, 'ogc_request': 'GetCapabilities', 'ogc_service': 'wms','ogc_version': '1.1.1','registering_with_group': 1,'registering_for_other_organization': '','uri': url}, verify=False)
     print("registering: "+url)

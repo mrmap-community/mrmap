2d245923536eb5e44234bd8adbd1fb73beb378ce
peltriau <michel.peltriaux@vermkv.rlp.de>
2020-04-28 13:42:53 +0200

    Tests added
    * adds Mapskinner decorator tests
    * refactors baker recipe for superuser creation to create superuser named "Superuser" instead of "Testuser"
    * adds check_permission decorator test

--- tests/unit_tests/MapSkinner_app/test_decorator.py
+++ tests/unit_tests/MapSkinner_app/test_decorator.py
@@ -5,21 +5,27 @@ Contact: michel.peltriaux@vermkv.rlp.de
 Created on: 28.04.20
 
 """
+from django.contrib.messages.storage.fallback import FallbackStorage
+from django.contrib.sessions.middleware import SessionMiddleware
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from django.test import TestCase, Client, RequestFactory
 
-from MapSkinner.decorator import log_proxy
+from MapSkinner.decorator import log_proxy, check_permission
 from service.models import Metadata, ProxyLog
-from tests.baker_recipes.db_setup import create_superadminuser, create_wms_service
+from structure.models import Permission
+from tests.baker_recipes.db_setup import create_superadminuser, create_wms_service, create_testuser
 from tests.baker_recipes.structure_app.baker_recipes import PASSWORD
 
 TEST_URI = "http://test.com?request=GetTest"
 TEST_URI_LOG_STRING = "/?request=GetTest"
+MSG_PERMISSION_CHECK_FALSE_POSITIVE = "Testuser without permissions passed @check_permission"
+MSG_PERMISSION_CHECK_TRUE_NEGATIVE = "Testuser with permissions did not pass @check_permission"
 
 
 class DecoratorTestCase(TestCase):
     def setUp(self):
+        self.default_user = create_testuser()
         self.user = create_superadminuser()
         self.client = Client()
         self.client.login(username=self.user.username, password=PASSWORD)
@@ -43,7 +49,52 @@ class DecoratorTestCase(TestCase):
         Returns:
 
         """
-        pass
+        # Mock the usage of the decorator
+        @check_permission(
+            Permission(
+                can_create_organization=True
+            )
+        )
+        def test_function(request, *args, **kwargs):
+            return HttpResponse()
+
+        # Testuser permission check without any permissions must fail
+        # Mock the request
+        request = self.request_factory.get(
+            TEST_URI,
+        )
+        request.user = self.default_user
+
+        # add support for message middleware
+        session_middleware = SessionMiddleware()
+        session_middleware.process_request(request)
+        request.session.save()
+        request.META["HTTP_REFERER"] = "/"
+        # adding messages
+        messages = FallbackStorage(request)
+        setattr(request, '_messages', messages)
+
+        response = test_function(request)
+
+        # Expect a redirect
+        self.assertEqual(response.status_code, 302, msg=MSG_PERMISSION_CHECK_FALSE_POSITIVE)
+
+        # Give Testuser group permission
+        user_groups = self.default_user.get_groups()
+        first_group = user_groups.first()
+        first_group.role.permission.can_create_organization = True
+        first_group.role.permission.save()
+
+        # Testuser permission check with permission must run
+        # Mock the request
+        response = test_function(request)
+
+        # Expect a 200
+        self.assertEqual(response.status_code, 200, msg=MSG_PERMISSION_CHECK_TRUE_NEGATIVE)
+
+        # Reset permission for other tests
+        first_group.role.permission.can_create_organization = False
+        first_group.role.permission.save()
 
     def test_log_proxy(self):
         """ Tests whether the responses of a service is properly logged

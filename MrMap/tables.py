0bd2aad2fbb3645ab4ce50a500f0f52efb6050c3
joki <jonas.kiefer@live.com>
2020-06-29 16:21:28 +0200

    Service subscription
    issue #335
    
    * refactors MrMapTables a bit

--- MrMap/tables.py
+++ MrMap/tables.py
@@ -15,42 +15,22 @@ from structure.models import Permission
 from users.helper import user_helper
 
 
-def prepare_table_pagination_settings(request: HttpRequest, table: django_tables2, param_lead: str):
-    return prepare_list_pagination_settings(request, list(table.rows), param_lead)
-
-
-def prepare_list_pagination_settings(request: HttpRequest, l: list, param_lead: str):
-    page_size_options = list(filter(lambda item: item <= len(l), PAGE_SIZE_OPTIONS))
-
-    if not page_size_options.__contains__(len(l)):
-        page_size_options.append(len(l))
-
-    page_size_options = list(filter(lambda item: item <= PAGE_SIZE_MAX, page_size_options))
-
-    pagination = {'page_size_param': param_lead + '-size',
-                  'page_size_options': page_size_options,
-                  'page_name': param_lead + '-page'
-                  }
-
-    if PAGE_SIZE_DEFAULT <= page_size_options[-1]:
-        page_size = PAGE_SIZE_DEFAULT
-    else:
-        page_size = page_size_options[-1]
-
-    pagination.update({'page_size': request.GET.get(pagination.get('page_size_param'), page_size)})
-
-    return pagination
-
-
 class MrMapTable(tables.Table):
     filter = None
-    pagination = None
+    pagination = {}
     page_field = None
     caption = ""
 
+    def __init__(self, request=None, *args, **kwargs):
+        super(MrMapTable, self).__init__(template_name=DJANGO_TABLES2_BOOTSTRAP4_CUSTOM_TEMPLATE, *args, **kwargs)
+        # Generate a random id for html template
+        self.table_id = ''.join(random.choice(string.ascii_lowercase) for i in range(10))
+        self.request = request
+        self.user = user_helper.get_user(request)
+
     def configure_pagination(self, request: HttpRequest, param_lead: str):
         RequestConfig(request).configure(self)
-        self.pagination = prepare_table_pagination_settings(request, self, param_lead)
+        self.prepare_table_pagination_settings(request, param_lead)
         self.page_field = self.pagination.get('page_name')
         self.paginate(page=request.GET.get(self.pagination.get('page_name'), PAGE_DEFAULT),
                       per_page=request.GET.get(self.pagination.get('page_size_param'), PAGE_SIZE_DEFAULT))
@@ -70,10 +50,25 @@ class MrMapTable(tables.Table):
         else:
             return ''
 
-    def __init__(self, request=None, *args, **kwargs):
-        super().__init__(template_name=DJANGO_TABLES2_BOOTSTRAP4_CUSTOM_TEMPLATE, *args, **kwargs)
-        # Generate a random id for html template
-        self.table_id = ''.join(random.choice(string.ascii_lowercase) for i in range(10))
-        self.request = request
-        self.user = user_helper.get_user(request)
+    def prepare_table_pagination_settings(self, request: HttpRequest, param_lead: str):
+        return self.prepare_list_pagination_settings(request, list(self.rows), param_lead)
 
+    def prepare_list_pagination_settings(self, request: HttpRequest, l: list, param_lead: str):
+        page_size_options = list(filter(lambda item: item <= len(l), PAGE_SIZE_OPTIONS))
+
+        if not page_size_options.__contains__(len(l)):
+            page_size_options.append(len(l))
+
+        page_size_options = list(filter(lambda item: item <= PAGE_SIZE_MAX, page_size_options))
+
+        self.pagination = {'page_size_param': param_lead + '-size',
+                           'page_size_options': page_size_options,
+                           'page_name': param_lead + '-page'
+                           }
+
+        if PAGE_SIZE_DEFAULT <= page_size_options[-1]:
+            page_size = PAGE_SIZE_DEFAULT
+        else:
+            page_size = page_size_options[-1]
+
+        self.pagination.update({'page_size': request.GET.get(self.pagination.get('page_size_param'), page_size)})

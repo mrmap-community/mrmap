3c2a2806b982b385ae03aa59328793bcf4eea03d
mipel <hadunan@gmail.com>
2020-09-03 13:56:06 +0200

    Filter for Access Form
    * adds unit_test (mg, mog)

--- tests/unit_tests/editor_app/test_filters.py
+++ tests/unit_tests/editor_app/test_filters.py
@@ -5,13 +5,15 @@ Contact: michel.peltriaux@vermkv.rlp.de
 Created on: 24.04.20
 
 """
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 
 from editor.filters import EditorAccessFilter
 from service.filters import MetadataWmsFilter, MetadataWfsFilter
 from service.helper.enums import OGCServiceEnum
 from service.models import Metadata
-from tests.baker_recipes.db_setup import create_superadminuser, create_wms_service, create_wfs_service
+from structure.models import MrMapGroup
+from tests.baker_recipes.db_setup import create_superadminuser, create_wms_service, create_wfs_service, \
+    create_guest_groups
 from tests.utils import check_filtering
 
 
@@ -21,6 +23,7 @@ class EditorFiltersTestCase(TestCase):
         self.user = create_superadminuser()
 
         self.user_groups = self.user.get_groups()
+        self.guest_groups = create_guest_groups(how_much_groups=10)
 
         create_wms_service(group=self.user.get_groups().first(), how_much_services=10)
         create_wfs_service(group=self.user.get_groups().first(), how_much_services=10)
@@ -59,12 +62,13 @@ class EditorFiltersTestCase(TestCase):
         )
         self.assertTrue(result, msg="MetadataWfsFilter not filtered properly")
 
-    def test_editor_access_group_filtering(self):
+    def test_editor_access_group_filtering_by_name(self):
         """ Tests the EditorAccessFilter functionality
 
         Returns:
 
         """
+        # Check name filtering
         result = check_filtering(
             EditorAccessFilter,
             "q",
@@ -72,3 +76,56 @@ class EditorFiltersTestCase(TestCase):
             self.user_groups
         )
         self.assertTrue(result, msg="EditorAccessFilter not filtered properly")
+
+    def test_editor_access_group_filtering_user_groups(self):
+        """ Tests the EditorAccessFilter functionality
+
+        Returns:
+
+        """
+        # Check user group filtering
+        all_groups = MrMapGroup.objects.all()
+
+        # Create an instance of a GET request.
+        request_factory = RequestFactory()
+        request = request_factory.get('/')
+        request.user = self.user
+
+        filtered_qs = EditorAccessFilter(
+            {
+                "mg": True
+            },
+            all_groups,
+            request=request
+        ).qs
+
+        self.assertLess(filtered_qs.count(), all_groups.count())
+        self.assertEqual(filtered_qs.count(), self.user_groups.count())
+
+    def test_editor_access_group_filtering_user_org_groups(self):
+        """ Tests the EditorAccessFilter functionality
+
+        Returns:
+
+        """
+        # Check user group filtering
+        all_groups = MrMapGroup.objects.all()
+
+        # Create an instance of a GET request.
+        request_factory = RequestFactory()
+        request = request_factory.get('/')
+        request.user = self.user
+
+        filtered_qs = EditorAccessFilter(
+            {
+                "mog": True
+            },
+            all_groups,
+            request=request
+        ).qs
+        org_groups = all_groups.filter(
+            organization=self.user.organization
+        )
+
+        self.assertLess(filtered_qs.count(), all_groups.count())
+        self.assertEqual(filtered_qs.count(), org_groups.count())

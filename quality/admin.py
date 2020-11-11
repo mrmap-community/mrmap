56e68829813bedd3ed344dc17baf79489d80c523
jansule <j_sule02@uni-muenster.de>
2020-11-11 09:43:22 +0100

    update admin

--- quality/admin.py
+++ quality/admin.py
@@ -7,12 +7,13 @@ Created on: 27.10.20
 """
 from django.contrib import admin
 
-from quality.models import ConformityCheckConfigurationExternal, ConformityCheckConfigurationInternal, Rule, RuleSet, \
+from quality.models import ConformityCheckConfigurationExternal, \
+    ConformityCheckConfigurationInternal, Rule, RuleSet, \
     ConformityCheckRun
 
 
 class ConformityCheckConfigurationExternalAdmin(admin.ModelAdmin):
-    list_display = ('id',)
+    list_display = ('id', 'name', 'metadata_types')
 
 
 class ConformityCheckConfigurationInternalAdmin(admin.ModelAdmin):
@@ -32,19 +33,12 @@ class RuleSetAdmin(admin.ModelAdmin):
 # TODO remove this. This is just for testing purposes
 class ConformityCheckRunAdmin(admin.ModelAdmin):
     list_display = ('id', 'metadata', 'time_start', 'time_stop', 'passed')
-    # metadata = models.ForeignKey(Metadata, on_delete=models.CASCADE)
-    # conformity_check_configuration = models.ForeignKey(ConformityCheckConfiguration, on_delete=models.CASCADE)
-    # time_start = models.DateTimeField(blank=True, null=True)
-    # time_stop = models.DateTimeField(blank=True, null=True)
-    # errors = models.TextField(blank=True, null=True)
-    # passed = models.BooleanField(blank=True, null=True)
-    # additional_info = models.TextField(blank=True, null=True)
-    # result = models.TextField(blank=True, null=True)
 
 
-admin.site.register(ConformityCheckConfigurationExternal, ConformityCheckConfigurationExternalAdmin)
-admin.site.register(ConformityCheckConfigurationInternal, ConformityCheckConfigurationInternalAdmin)
+admin.site.register(ConformityCheckConfigurationExternal,
+                    ConformityCheckConfigurationExternalAdmin)
+admin.site.register(ConformityCheckConfigurationInternal,
+                    ConformityCheckConfigurationInternalAdmin)
 admin.site.register(Rule, RuleAdmin)
 admin.site.register(RuleSet, RuleSetAdmin)
-# TODO remove this. This is just for testing purposes
 admin.site.register(ConformityCheckRun, ConformityCheckRunAdmin)

6971e79926cb4cfaf0a7c813e86b5e6ceb1f79a1
hollsandre <andre.holl@vermkv.rlp.de>
2021-03-23 14:30:37 +0100

    fixed paths for install and configs and adjusted documentation

--- docs/installation/4-mrmap.md
+++ docs/installation/4-mrmap.md
@@ -11,70 +11,71 @@ Begin by installing all system packages required by MrMap and its dependencies.
 
 ### Debian
 
+On debian 10 you have python3.7 available in the system packages, on other versions
+ or distros you might have to compile it yourself.
+
 ```no-highlight
 sudo apt install -y libcurl4-openssl-dev libssl-dev python3.7-dev gdal-bin virtualenv
 ```
 
-!!! note
-    To use the virtualenv run: source `PATH-TO-MrMap`/venv/bin/activate
-
-
-Before continuing with either platform, update pip (Python's package management tool) to its latest release:
-
-```no-highlight
-sudo pip3 install --upgrade pip
-```
-
-### Initial setup Mr. Map
-1. activate your configured virtualenv:
-        
-        $ source `PATH-TO-YOUR-VENV`/bin/activate
+### Initial setup Mr. Map (virtualenv way)
 
 1. clone the project from the repo to your preferred install directory:
-        
-        (venv) $ cd `INSTALL-DIR`
-        (venv) $ git clone https://git.osgeo.org/gitea/GDI-RP/MrMap 
-   
-!!! note
-    all following commands are from within the project root directory run.
 
-1. install all requirements:
+        cd `INSTALL-DIR`, e.g: /opt/
+        git clone https://github.com/mrmap-community/mrmap
 
-        (venv) $ pip install -r requirements.txt
-        
+1. create your virtualenv and activate it:
+
+        virtualenv -ppython3.7 `PATH-TO-YOUR-VENV`, e.g: /opt/mrmap/venv
+        source `PATH-TO-YOUR-VENV`/bin/activate
+
+1. install all requirements in to the virtualenv:
+
+        (venv) $ python -m pip install -r `INSTALL-DIR`/mrmap/requirements.txt
+
+1. set database parameters in `INSTALL-DIR`/mrmap/MrMap/sub_settings/db_settings.py
+
+        ...
+        'USER': 'mrmap',
+        'PASSWORD': 'J5brHrAXFLQSif0K',
+        ...
+
 1. run django migrations:
 
-        (venv) $ python manage.py migrate
+        (venv) $ python `INSTALL-DIR`/mrmap/manage.py migrate
 
 1. (Optional) Configure proxy:
-    
+
     Make sure the HTTP_PROXY variable in MrMap/settings.py is set correctly for your system
 
 1. run setup routine to get initialized db with admin user for mr. map:
-        
-        (venv) $ python manage.py setup
-        
+
+        (venv) $ python `INSTALL-DIR`/mrmap/manage.py setup
+
 ### Start up everything we need
 !!! note
     all following commands are run within the project root directory run by using your virtual env.
-       
+    you can run the commands in background by adding a trailing &
+
 1. start up celery process (celery will do async jobs for us)
 
+        (venv) $ cd  `INSTALL-DIR`/mrmap/
         (venv) $ celery -A MrMap worker -l INFO
-        
-1. start up mr. map
-
-        (venv) $ python manage.py runserver_plus
-!!! note
-   [runserver_plus](https://django-extensions.readthedocs.io/en/latest/runserver_plus.html) gives us more debug informations
 
 1. start up celery beat process
 
         (venv) $ celery -A MrMap beat -l INFO
-   
 
-1. you should see the login page after opening http://127.0.0.1:8000:
+1. start up mr. map
+
+        (venv) $ python manage.py runserver_plus 0.0.0.0:8000
+!!! note
+   [runserver_plus](https://django-extensions.readthedocs.io/en/latest/runserver_plus.html) gives us more debug informations
+
+
+1. you should see the login page after opening http://127.0.0.1:8000 or http://YOUR-IP-ADDRESS:8000:
 
     ![login page](../installation/mrmap_loginpage.png)
-    
+
 You can now login with the user you configured in your python manage.py setup routine.
--- install/mrmap_production_setup.bash
+++ install/mrmap_production_setup.bash
@@ -31,27 +31,22 @@ sed -i s/"host    all             all             127.0.0.1\/32            md5"/
 
 # MrMap setup, has to be done as postgres because of postgis extension
 
-git clone https://git.osgeo.org/gitea/GDI-RP/MrMap /opt/MrMap
+git clone https://github.com/mrmap-community/mrmap/ /opt/mrmap
 #python -m pip install --upgrade pip
 python -m pip install uwsgi flower
 python -m pip install -r /opt/MrMap/requirements.txt
-python /opt/MrMap/manage.py makemigrations service
-python /opt/MrMap/manage.py makemigrations structure
-python /opt/MrMap/manage.py makemigrations users
-python /opt/MrMap/manage.py makemigrations monitoring
-python /opt/MrMap/manage.py makemigrations editor
-python /opt/MrMap/manage.py migrate
-python /opt/MrMap/manage.py collectstatic
+python /opt/mrmap/mrmap/manage.py migrate
+python /opt/mrmap/mrmap/manage.py collectstatic
 
 # changes to settings.py, set Django debug to false, set hostname, enable ssl
-sed -i s/"DEBUG = True"/"DEBUG = False"/g /opt/MrMap/MrMap/sub_settings/django_settings.py
-sed -i s/"HOST_NAME = \"127.0.0.1:8000\""/"HOST_NAME = \"$hostname\""/g /opt/MrMap/MrMap/sub_settings/dev_settings.py
-sed -i s/"HTTP_OR_SSL = \"http:\/\/\""/"HTTP_OR_SSL = \"https:\/\/\""/g /opt/MrMap/MrMap/sub_settings/dev_settings.py
-sed -i s-"HTTP_PROXY = \"\""-"HTTP_PROXY = \"$proxy\""-g /opt/MrMap/MrMap/settings.py
+sed -i s/"DEBUG = True"/"DEBUG = False"/g /opt/mrmap/mrmap/MrMap/sub_settings/django_settings.py
+sed -i s/"HOST_NAME = \"127.0.0.1:8000\""/"HOST_NAME = \"$hostname\""/g /opt/mrmap/mrmap/MrMap/sub_settings/dev_settings.py
+sed -i s/"HTTP_OR_SSL = \"http:\/\/\""/"HTTP_OR_SSL = \"https:\/\/\""/g /opt/mrmap/mrmap/MrMap/sub_settings/dev_settings.py
+sed -i s-"HTTP_PROXY = \"\""-"HTTP_PROXY = \"$proxy\""-g /opt/mrmap/mrmap/MrMap/settings.py
 # generate new secret key
 skey=`python /opt/MrMap/manage.py shell -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())"`
-sed -i '/^SECRET_KEY/d' /opt/MrMap/MrMap/sub_settings/django_settings.py
-sed -i "/# SECURITY WARNING: keep the secret key used in production secret\!/a SECRET_KEY = '$skey'" /opt/MrMap/MrMap/sub_settings/django_settings.py
+sed -i '/^SECRET_KEY/d' /opt/mrmap/mrmap/MrMap/sub_settings/django_settings.py
+sed -i "/# SECURITY WARNING: keep the secret key used in production secret\!/a SECRET_KEY = '$skey'" /opt/mrmap/mrmap/MrMap/sub_settings/django_settings.py
 # remove postgres trust, replace with mr map database user
 sed -i s/"host    all             all             127.0.0.1\/32            trust"/"host    MrMap             $mrmap_db_user             127.0.0.1\/32            md5"/g /etc/postgresql/11/main/pg_hba.conf
 
@@ -59,10 +54,10 @@ sed -i s/"host    all             all             127.0.0.1\/32            trust
 
 
 # change settings.py to dedicated user
-sed -i s/"        'USER': 'postgres',"/"        'USER': '$mrmap_db_user',"/g /opt/MrMap/MrMap/sub_settings/db_settings.py
+sed -i s/"        'USER': 'postgres',"/"        'USER': '$mrmap_db_user',"/g /opt/mrmap/mrmap/MrMap/sub_settings/db_settings.py
 
-if  ! grep -q "        'PASSWORD': '$mrmap_db_pw',"  /opt/MrMap/MrMap/sub_settings/db_settings.py ;then
-	sed -i "/        'USER': '$mrmap_db_user',/a \        \'PASSWORD': '$mrmap_db_pw'," /opt/MrMap/MrMap/sub_settings/db_settings.py
+if  ! grep -q "        'PASSWORD': '$mrmap_db_pw',"  /opt/mrmap/mrmap/MrMap/sub_settings/db_settings.py ;then
+	sed -i "/        'USER': '$mrmap_db_user',/a \        \'PASSWORD': '$mrmap_db_pw'," /opt/mrmap/mrmap/MrMap/sub_settings/db_settings.py
 fi
 
 # db setup done
@@ -84,7 +79,7 @@ if [ ! -f /etc/ssl/certs/dhparams.pem ]; then
 fi
 
 # copy nginx config
-cp -a /opt/MrMap/install/confs/mrmap_nginx /etc/nginx/conf.d/mrmap.conf
+cp -a /opt/mrmap/mrmap/install/confs/mrmap_nginx /etc/nginx/conf.d/mrmap.conf
 # replace hostname in nginx config
 sed -i s/"    server_name 127.0.0.1;"/"    server_name $hostname;"/g /etc/nginx/conf.d/mrmap.conf
 sed -i s/"    server_name         127.0.0.1;"/"    server_name         $hostname;"/g /etc/nginx/conf.d/mrmap.conf
@@ -96,10 +91,10 @@ fi
 rm /etc/nginx/conf.d/default.conf
 
 # copy uwsgi ini
-cp -a /opt/MrMap/install/confs/mrmap_uwsgi_ini /opt/MrMap/MrMap/mrmap_uwsgi.ini
+cp -a /opt/mrmap/mrmap/install/confs/mrmap_uwsgi_ini /opt/mrmap/mrmap/MrMap/mrmap_uwsgi.ini
 
 # copy script to change database rights
-cp -a /opt/MrMap/install/confs/change_db_rights /tmp/change_database_rights.psql
+cp -a /opt/mrmap/mrmap/install/confs/change_db_rights /tmp/change_database_rights.psql
 
 sed -i s/\$mrmap_db_user/$mrmap_db_user/g /tmp/change_database_rights.psql
 # execute
@@ -107,7 +102,7 @@ su - postgres -c "psql -d 'MrMap' -f /tmp/change_database_rights.psql"
 
 
 # copy uwsgi systemd config
-cp -a /opt/MrMap/install/confs/mrmap_uwsgi_service /etc/systemd/system/uwsgi.service
+cp -a /opt/mrmap/mrmap/install/confs/mrmap_uwsgi_service /etc/systemd/system/uwsgi.service
 
 # start uwsgi and enable start on boot
 systemctl start uwsgi
@@ -125,10 +120,10 @@ chown www-data:www-data /var/run/celery/
 
 
 # copy celery environment file
-cp -a /opt/MrMap/install/confs/mrmap_celery_environment /etc/default/celery
+cp -a /opt/mrmap/mrmap/install/confs/mrmap_celery_environment /etc/default/celery
 
 # copy celery service file
-cp -a /opt/MrMap/install/confs/mrmap_celery_service /etc/systemd/system/celery.service
+cp -a /opt/mrmap/mrmap/install/confs/mrmap_celery_service /etc/systemd/system/celery.service
 
 # start celery and enable start on boot
 systemctl start celery
@@ -136,11 +131,11 @@ systemctl enable celery
 
 
 # copy celery helper service file
-cp -a /opt/MrMap/install/confs/mrmap_celery_helper_service /etc/systemd/system/celery-helper.service
+cp -a /opt/mrmap/mrmap/install/confs/mrmap_celery_helper_service /etc/systemd/system/celery-helper.service
 systemctl enable celery-helper
 
 # copy celery flower statistics service file
-cp -a /opt/MrMap/install/confs/mrmap_celery_flower_service /etc/systemd/system/celery-flower.service
+cp -a /opt/mrmap/mrmap/install/confs/mrmap_celery_flower_service /etc/systemd/system/celery-flower.service
 systemctl enable celery-flower
 systemctl start celery-flower
 
@@ -161,14 +156,14 @@ This can take up to 1 hour, recommended for production!y/n? \n
 You can do this later with bash /opt/MrMap/install/modsecurity_and_stronger_DH.bash " yn
     case $yn in
         [Yy]* )
-				bash /opt/MrMap/install/modsecurity_and_stronger_DH.bash;
+				bash /opt/mrmap/mrmap/install/modsecurity_and_stronger_DH.bash;
         break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
 echo "Executing initial setup"
-python /opt/MrMap/manage.py setup
+python /opt/mrmap/mrmap/manage.py setup
 chown -R www-data /opt/MrMap/logs
 
 /etc/init.d/nginx restart

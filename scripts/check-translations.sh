d2c97dc3afc03b0964c84cec2c69f862f9b73870
jonas <jonas@pc-jonas>
2021-04-20 17:09:39 +0200

    * fix translations * add check script for empty translations

---
+++ scripts/check-translations.sh
@@ -0,0 +1,13 @@
+case `pcregrep -Mn 'msgstr "".*\n\n' ../mrmap/locale/de/LC_MESSAGES/django.po >/dev/null; echo $?` in
+  0)
+    echo "empty translation found."
+    exit 1
+    ;;
+  1)
+    exit 0
+    ;;
+  *)
+    echo "something went wrong."
+    exit 1
+    ;;
+esac

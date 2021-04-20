7b4c593cad0fdadeb2016fc95c423467acdca1b1
jonas <jonas@pc-jonas>
2021-04-20 11:08:34 +0200

    update setup command

--- mrmap/MrMap/management/commands/setup.py
+++ mrmap/MrMap/management/commands/setup.py
@@ -10,7 +10,8 @@ from getpass import getpass
 from dateutil.parser import parse
 from django.contrib.auth import get_user_model
 from django.core.management import BaseCommand, call_command
-from django.db import transaction
+from django.db import transaction, connections, DEFAULT_DB_ALIAS, OperationalError
+from django.db.migrations.executor import MigrationExecutor
 from django.utils import timezone
 
 from monitoring.settings import MONITORING_REQUEST_TIMEOUT, MONITORING_TIME
@@ -24,15 +25,44 @@ class Command(BaseCommand):
     help = "Runs an initial setup for creating the superuser on a fresh installation."
 
     def add_arguments(self, parser):
-        pass
+        parser.add_argument('--reset', dest='reset', action='store_true', help="calls reset_db command in front of setup routine.")
+        parser.add_argument('--reset-force', dest='reset_force', action='store_true', help="calls reset_db command with --noinput arg in front of setup routine.")
+
+        parser.set_defaults(reset=False)
+        parser.set_defaults(reset_force=False)
 
     def handle(self, *args, **options):
+        if options['reset_force']:
+            call_command('reset_db', '-c', '--noinput')
+        elif options['reset']:
+            call_command('reset_db', '-c')
         with transaction.atomic():
+            self._pre_setup()
             # sec run the main setup
             self._run_superuser_default_setup()
             # then load the default categories
             call_command('load_categories')
             call_command('load_licences')
+
+    def _is_database_synchronized(self, database):
+        connection = connections[database]
+        try:
+            connection.prepare_database()
+        except OperationalError:
+            connection.connect()
+            connection.prepare_database()
+        executor = MigrationExecutor(connection)
+        targets = executor.loader.graph.leaf_nodes()
+        return not executor.migration_plan(targets)
+
+    def _pre_setup(self):
+        """ check if there are pending migrations. If so, we migrate them"""
+        if self._is_database_synchronized(DEFAULT_DB_ALIAS):
+            # All migrations have been applied.
+            pass
+        else:
+            call_command('migrate')
+        call_command('create_roles')
 
     def _run_superuser_default_setup(self):
         """ Encapsules the main setup for creating all default objects and the superuser

f90792898d8d05661f5da0c2a973ce36df15b057
jonas <jonas@pc-jonas>
2021-04-22 08:48:10 +0200

    finish translation check script

--- scripts/check-translations.sh
+++ scripts/check-translations.sh
@@ -2,18 +2,14 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd $parent_path
 
-python ../mrmap/manage.py dev_messages
-
 case `grep -zoP 'msgstr "".*\n\n' ../mrmap/locale/de/LC_MESSAGES/django.po >/dev/null; echo $?` in
   0)
-    echo "empty translation found."
     exit 1
     ;;
   1)
     exit 0
     ;;
   *)
-    echo "something went wrong."
     exit 1
     ;;
 esac

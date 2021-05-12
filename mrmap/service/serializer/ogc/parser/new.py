5b5cd1432b57d04404e6c8b8806d6516f0b68fa7
joki <jonas.kiefer@live.com>
2021-05-12 15:44:15 +0200

    * implement xmlmap parser for wms service metadata object

--- mrmap/service/serializer/ogc/parser/new.py
+++ mrmap/service/serializer/ogc/parser/new.py
@@ -4,14 +4,10 @@ from eulxml import xmlmap
 from django.apps import apps
 from django.db import models
 
-class OGCWebMapServiceMetadata(xmlmap.XmlObject):
-    name = xmlmap.StringField(xpath='Name')
-    title = xmlmap.StringField(xpath='Title')
-    abstract = xmlmap.StringField(xpath='Abstract')
-    fees = xmlmap.StringField(xpath='Fees')
-    access_constraints = xmlmap.StringField(xpath='AccessConstraints')
+GENERIC_NAMESPACE_TEMPLATE = "*[local-name()='{}']"
 
-    #keywords = xmlmap.StringListField('KeywordList/Keyword')
+
+class DBModelConverterMixin:
     class Meta:
         model = 'service.Metadata'
 
@@ -26,8 +22,37 @@ class OGCWebMapServiceMetadata(xmlmap.XmlObject):
         return self._get_model_class(**self.__dict__)
 
 
+class Keyword(xmlmap.XmlObject):
+    keyword = xmlmap.StringField(xpath="*[local-name()='..']")
+
+
+class ServiceMetadataContact(xmlmap.XmlObject):
+    name = xmlmap.StringField(xpath="*[local-name()='ContactPersonPrimary']/*[local-name()='ContactOrganization']")
+    person_name = xmlmap.StringField(xpath="*[local-name()='ContactPersonPrimary']/*[local-name()='ContactPerson']")
+    phone = xmlmap.StringField(xpath="*[local-name()='ContactVoiceTelephone']")
+    facsimile = xmlmap.StringField(xpath="*[local-name()='ContactFacsimileTelephone']")
+    email = xmlmap.StringField(xpath="*[local-name()='ContactElectronicMailAddress']")
+    country = xmlmap.StringField(xpath="*[local-name()='ContactAddress']/*[local-name()='Country']")
+    postal_code = xmlmap.StringField(xpath="*[local-name()='ContactAddress']/*[local-name()='PostCode']")
+    city = xmlmap.StringField(xpath="*[local-name()='ContactAddress']/*[local-name()='City']")
+    state_or_province = xmlmap.StringField(xpath="*[local-name()='ContactAddress']/*[local-name()='StateOrProvince']")
+    address = xmlmap.StringField(xpath="*[local-name()='ContactAddress']/*[local-name()='Address']")
+
+
+class ServiceMetadata(xmlmap.XmlObject):
+    name = xmlmap.StringField(xpath="*[local-name()='Name']")
+    title = xmlmap.StringField(xpath="*[local-name()='Title']")
+    abstract = xmlmap.StringField(xpath="*[local-name()='Abstract']")
+    fees = xmlmap.StringField(xpath="*[local-name()='Fees']")
+    access_constraints = xmlmap.StringField(xpath="*[local-name()='AccessConstraints']")
+    online_resource = xmlmap.StringField(xpath="*[local-name()='OnlineResource']/@*[local-name()='href']")
+
+    keywords = xmlmap.NodeListField(xpath="*[local-name()='KeywordList']/*[local-name()='Keyword']", node_class=Keyword)
+    contact = xmlmap.NodeField(xpath="*[local-name()='ContactInformation']", node_class=ServiceMetadataContact)
+
+
 class Service(xmlmap.XmlObject):
-    service_metadata = xmlmap.NodeField(xpath='Service', node_class=OGCWebMapServiceMetadata)
+    service_metadata = xmlmap.NodeField(xpath="*[local-name()='Service']", node_class=ServiceMetadata)
 
 
 if __name__ == '__main__':

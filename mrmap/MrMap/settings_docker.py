2c4ca2e82bde4cdb26b7cfe2c1f94f924ad18485
Jonas <jonas@jonas-ThinkPad-S1-Yoga-12>
2021-09-07 15:12:34 +0200

    Update mrmap-docker.run.xml, docker-compose-dev.yml, and 3 more files...

--- mrmap/MrMap/settings_docker.py
+++ mrmap/MrMap/settings_docker.py
@@ -17,14 +17,18 @@ ALLOWED_HOSTS.append("172.17.0.1")
 DATABASES['default']['NAME'] = 'mrmap'
 DATABASES['default']['USER'] = 'mrmap'
 DATABASES['default']['PASSWORD'] = 'mrmap'
-DATABASES['default']['PORT'] = '5555'
+DATABASES['default']['PORT'] = '5432'
+DATABASES['default']['HOST'] = 'mrmap-postgis'
 
 CACHES['default']['LOCATION'] = 'redis://localhost:5556/1'
 
-BROKER_URL = f'redis://localhost:5556'
+BROKER_URL = f'redis://mrmap-redis:6379'
 # CELERY_RESULT_BACKEND = 'redis://localhost:5556'
 
 CHANNEL_LAYERS['default']['CONFIG']['hosts'] = [('localhost', '5556')]
 
 MAPSERVER_LOCAL_PATH = "http://127.0.0.1:5557/cgi-bin/mapserv?"
 MAPSERVER_SECURITY_MASK_FILE_PATH = "/etc/mapserver/security_mask.map"
+
+
+pass

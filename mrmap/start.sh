ac981341f22e8368381058cbf21d15ae2f876ea1
Jonas <jonas@jonas-ThinkPad-S1-Yoga-12>
2021-09-08 16:15:32 +0200

    Update mrmap-docker.run.xml, docker-compose-dev.yml, and 4 more files...

--- mrmap/start.sh
+++ mrmap/start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+cd /opt/mrmap
+
+#python manage.py makemigrations
+#python manage.py setup
+
 # start gunicorn as wsgi service
-gunicorn MrMap.wsgi:application -b 0.0.0.0:8001 --workers 4 &
+gunicorn MrMap.wsgi:application -b 0.0.0.0:8001 --workers 4
 
 # start uvicorn as asgi service
 uvicorn --host 0.0.0.0 --port 8002 --workers 4  MrMap.asgi:application

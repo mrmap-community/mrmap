2eeb0095cc58e4d0447373dd4537bd3d86579f8a
jokiefer <jonas.kiefer@live.com>
2021-09-10 17:04:41 +0200

    Update start.sh

--- mrmap/start.sh
+++ mrmap/start.sh
@@ -6,7 +6,7 @@ cd /opt/mrmap
 # start gunicorn as wsgi service
 if [ "$DJANGO_DEBUG" == "1" ]
 then
-  gunicorn -b 0.0.0.0:8001 --workers=2 --reload --log-level='debug' MrMap.wsgi:application &
+  gunicorn -b 0.0.0.0:8001 --workers=2 --reload --log-level='debug' --timeout=0 MrMap.wsgi:application &
 else
   gunicorn -b 0.0.0.0:8001 --workers=4 --log-level='error' MrMap.wsgi:application &
 fi
@@ -19,21 +19,7 @@ else
   echo "gunicorn successfully started."
 fi
 
-# start uvicorn as asgi service
-if [ "$DJANGO_DEBUG" == "1" ]
-then
-  uvicorn --host 0.0.0.0 --port 8002 --workers 2 --reload --log-level debug --proxy-headers --forwarded-allow-ips='*' MrMap.asgi:application &
-else
-  uvicorn --host 0.0.0.0 --port 8002 --workers 4 --log-level error --proxy-headers --forwarded-allow-ips='*' MrMap.asgi:application &
-fi
 
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start uvicorn: $status"
-  exit $status
-else
-  echo "uvicorn successfully started."
-fi
 
 
 # todo

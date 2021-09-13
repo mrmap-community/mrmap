0660a7f0c0578c34a51034ce710d63f20f3c6929
jokiefer <jonas.kiefer@live.com>
2021-09-13 09:21:20 +0200

    Update Dockerfile, startup.sh, and 2 more files...

--- mrmap/startup.sh
+++ mrmap/startup.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 #
 # Start all needed application services of MrMap.
-cd /opt/mrmap
+cd /opt/mrmap || exit
 
 # start gunicorn as wsgi service
 ./startup_gunicorn.sh &
 
 # start uvicorn as asgi service
-./startup_uvicorn &
+./startup_uvicorn.sh &
 
-# todo
 #  start celery worker
+#celery -A MrMap worker -E -l DEBUG -n default -Q default
+#celery -A MrMap worker -E -l DEBUG -n harvest -Q harvest
+#celery -A MrMap worker -E -l DEBUG -n download2 -Q download_described_elements
+#celery -A MrMap worker -E -l DEBUG -n download1 -Q download_iso_metadata
 
 # endless loop to keep docker container running endless
 while true; do sleep 30; done;
--- mrmap/startup_gunicorn.sh
+++ mrmap/startup_gunicorn.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cd /opt/mrmap
+cd /opt/mrmap || exit
 if [ "$DJANGO_DEBUG" == "1" ]
 then
-  gunicorn -b 0.0.0.0:8001 --workers=2 --reload --log-level='debug' --timeout=0 MrMap.wsgi:application &
+  gunicorn -b 0.0.0.0:8001 --workers=2 --reload --log-level='debug' --timeout=0 MrMap.wsgi:application
 else
-  gunicorn -b 0.0.0.0:8001 --workers=4 --log-level='error' MrMap.wsgi:application &
+  gunicorn -b 0.0.0.0:8001 --workers=4 --log-level='error' MrMap.wsgi:application
 fi

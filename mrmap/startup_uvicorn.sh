229b37dbb01cba1a073e3edf61b2d2b5fa03190b
Jonas <jonas@jonas-ThinkPad-S1-Yoga-12>
2021-09-13 16:16:59 +0200

    Update .env.dev, launch.json, and 9 more files...

--- mrmap/startup_uvicorn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /opt/mrmap || exit
-if [[ "$DJANGO_DEBUG" == "1" ]]
-then
-  uvicorn --host 0.0.0.0 --port 8002 --workers 2 --reload --log-level debug --proxy-headers --forwarded-allow-ips='*' MrMap.asgi:application
-else
-  uvicorn --host 0.0.0.0 --port 8002 --workers 4 --log-level error --proxy-headers --forwarded-allow-ips='*' MrMap.asgi:application
-fi

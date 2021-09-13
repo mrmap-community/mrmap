537dbe614e8edad638d231ea1fe10337d51dc14b
jokiefer <jonas.kiefer@live.com>
2021-09-13 12:42:24 +0200

    Update .env.dev, gunicorn.run.xml, and 7 more files...

--- mrmap/startup_uvicorn.sh
+++ mrmap/startup_uvicorn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /opt/mrmap || exit
-if [ "$DJANGO_DEBUG" == "1" ]
+if [[ "$DJANGO_DEBUG" == "1" ]]
 then
   uvicorn --host 0.0.0.0 --port 8002 --workers 2 --reload --log-level debug --proxy-headers --forwarded-allow-ips='*' MrMap.asgi:application
 else

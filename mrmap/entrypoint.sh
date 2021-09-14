a00154d750b76e7891d52ee2b59cadf81bfc0fb5
jokiefer <jonas.kiefer@live.com>
2021-09-14 12:45:59 +0200

    Update docker-compose.yml, Dockerfile, and 3 more files...

--- mrmap/entrypoint.sh
+++ mrmap/entrypoint.sh
@@ -36,9 +36,9 @@ if [[ "$START_UVICORN" != "False" ]];
 then
     if [[ "$DJANGO_DEBUG" == "1" ]];
     then
-    uvicorn --host 0.0.0.0 --port 8002 --workers 2 --reload --log-level 'debug' --proxy-headers --forwarded-allow-ips='*' MrMap.asgi:application &
+        uvicorn --host 0.0.0.0 --port 8002 --workers 2 --reload --log-level 'debug' --proxy-headers --forwarded-allow-ips='*' MrMap.asgi:application &
     else
-    uvicorn --host 0.0.0.0 --port 8002 --workers 4 --log-level 'error' --proxy-headers --forwarded-allow-ips='*' MrMap.asgi:application &
+        uvicorn --host 0.0.0.0 --port 8002 --workers 4 --log-level 'error' --proxy-headers --forwarded-allow-ips='*' MrMap.asgi:application &
     fi
 fi
 

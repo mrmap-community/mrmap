11f5c0ebd71e73175ab937eabdef98a188a24254
Jonas <jonas@jonas-ThinkPad-S1-Yoga-12>
2021-09-20 13:30:09 +0200

    Update environment.py

--- mrmap/main/tests/acceptance/environment.py
+++ mrmap/main/tests/acceptance/environment.py
@@ -1,4 +1,19 @@
-[behave]
-paths = tests/acceptance
-junit_directory = tests/reports
-junit = yes
+
+from selenium import webdriver
+
+def before_all(context):
+	# PhantomJS is used there (headless browser - meaning we can execute tests in a command-line environment, which is what we want for use with SemaphoreCI
+	# For debugging purposes, you can use the Firefox driver instead.
+
+	context.browser = webdriver.PhantomJS()
+	context.browser.implicitly_wait(1)
+	context.server_url = 'http://localhost'
+
+def after_all(context):
+	# Explicitly quits the browser, otherwise it won't once tests are done
+	context.browser.quit()
+
+def before_feature(context, feature):
+	# Code to be executed each time a feature is going to be tested
+	pass
+

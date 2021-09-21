bb463c32fb4b5e4d4f10c0240d3353064dddce5c
jokiefer <jonas.kiefer@live.com>
2021-09-21 10:57:49 +0200

    implement first behave test scenarios with selenium headless firefox

--- mrmap/main/tests/acceptance/environment.py
+++ mrmap/main/tests/acceptance/environment.py
@@ -1,19 +1,14 @@
+import behave_webdriver
 
-#from selenium import webdriver
 
 def before_all(context):
-	# PhantomJS is used there (headless browser - meaning we can execute tests in a command-line environment, which is what we want for use with SemaphoreCI
-	# For debugging purposes, you can use the Firefox driver instead.
-
-	#context.browser = webdriver.PhantomJS()
-	#context.browser.implicitly_wait(1)
-	#context.server_url = 'http://localhost'
-	print('before_all')
-	pass
+    context.behave_driver = behave_webdriver.Firefox.headless()
+    print('before_all')
+    pass
 
 def after_all(context):
 	# Explicitly quits the browser, otherwise it won't once tests are done
-	#context.browser.quit()
+	context.behave_driver.quit()
 	print('after_all')
 	pass
 

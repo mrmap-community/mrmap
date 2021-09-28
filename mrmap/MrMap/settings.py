fcfe315c3c6a9ddff27e5754261086db33c4152a
Jonas <jonas@jonas-ThinkPad-S1-Yoga-12>
2021-09-28 16:03:01 +0200

    * remove old test structure * implement behave test case example * update github action to run behave test cases

--- mrmap/MrMap/settings.py
+++ mrmap/MrMap/settings.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
     'leaflet',
     'breadcrumb',
     'mptt',
+    'behave_django',
 ]
 
 TEMPLATE_LOADERS = (
--- mrmap/acls/tests/test_signals.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from django.contrib.auth import get_user_model
-from django.contrib.contenttypes.models import ContentType
-from django.test import TestCase
-from guardian.core import ObjectPermissionChecker
-
-# todo
-

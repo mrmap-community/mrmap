4c1fc9aa56b0c63e619416c10d81f09673e3cca6
jokiefer <jonas.kiefer@live.com>
2021-10-05 09:49:51 +0200

    fix pre commit checks

--- mrmap/users/tests/acceptance/environment.py
+++ mrmap/users/tests/acceptance/environment.py
@@ -2,6 +2,7 @@ import behave_webdriver
 from selenium.webdriver.firefox.options import Options
 from datetime import datetime
 
+
 def before_all(context):
     options = Options()
     options.add_argument("--window-size=1920,1080")
@@ -9,9 +10,11 @@ def before_all(context):
 
     context.behave_driver = behave_webdriver.Firefox(options=options)
 
+
 def after_all(context):
     # Explicitly quits the browser, otherwise it won't once tests are done
     context.behave_driver.quit()
+
 
 def after_step(context, step):
     if step.status == "failed":

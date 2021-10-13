852112e1bec52aae86af5e24d45322f23ef11ddf
Unraveler <rui.nunes1985@gmail.com>
2021-10-13 16:59:25 +0200

    removed duplicated requirement

--- mrmap/registry/api/views/mapcontext.py
+++ mrmap/registry/api/views/mapcontext.py
@@ -28,4 +28,5 @@ class MapContextViewSet(ModelViewSet):
         """
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
+
         return Response(data=serializer.data, status=status.HTTP_200_OK)

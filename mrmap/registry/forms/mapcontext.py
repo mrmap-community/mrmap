dbaf002d76402b5020098124666b8f8727c1ba59
jokiefer <jonas.kiefer@live.com>
2021-10-14 11:59:58 +0200

    added empty form to template

--- mrmap/registry/forms/mapcontext.py
+++ mrmap/registry/forms/mapcontext.py
@@ -2,6 +2,7 @@ from django import forms
 from django.forms import ModelForm, HiddenInput
 from django.utils.translation import gettext_lazy as _
 from registry.models.mapcontext import MapContext, MapContextLayer
+from dal import autocomplete
 
 
 class MapContextForm(ModelForm):
@@ -19,8 +20,7 @@ class MapContextLayerForm(ModelForm):
         model = MapContextLayer
         widgets = {
             'parent': HiddenInput(),
-            # TODO: add autocomplete Widget to dataset_metadata
-            # 'dataset_metadata':  autocomplete.ModelSelect2(url='registry.autocomplete:dataset_metadata_ac'),
+            'dataset_metadata':  autocomplete.ModelSelect2(url='registry.autocomplete:dataset_metadata_ac'),
         }
         fields = [
             'id',

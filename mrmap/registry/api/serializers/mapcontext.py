5dcc5367bcdcf7272000a4bfdd9f9cee3315acfc
Markus Schneider <mschneider@terrestris.de>
2021-10-19 15:26:45 +0200

    MapContext: Implemented / documented serialization of the attributes defined for the GeoJSON encoding of the OWC:Offering class.

--- mrmap/registry/api/serializers/mapcontext.py
+++ mrmap/registry/api/serializers/mapcontext.py
@@ -5,67 +5,10 @@ from rest_framework.serializers import ModelSerializer, CharField, HyperlinkedMo
 from registry.models import MapContext, MapContextLayer
 
 
-class MapContextPropertiesSerializer(ModelSerializer):
-    lang = CharField(default='de')
-    subtitle = ModelField(model_field=MapContext()._meta.get_field('abstract'))
-    updated = ModelField(model_field=MapContext()._meta.get_field('last_modified_at'))
-    authors = SerializerMethodField()
-    publisher = SerializerMethodField()
-    creator = CharField(default='MrMap')  # format is unclear, see 7.1.1 vs 7.1.1.9
-    links = SerializerMethodField()
-
-    class Meta:
-        model = MapContext
-        fields = [
-            'lang',  # 7.1.1.2 language
-            'title',  # 7.1.1.4 title
-            'subtitle',  # 7.1.1.5 abstract
-            'updated',  # 7.1.1.6 updateDate (TODO: get rid of fractional seconds)
-            'authors',  # 7.1.1.7 author
-            'publisher',  # 7.1.1.8 publisher
-            'creator',  # 7.1.1.9 creator
-            # TODO 'rights', # 7.1.1.10 rights
-            # TODO 'categories', # 7.1.1.15 keyword
-            'links',  # 7.1.1.1 specReference + TODO: 7.1.1.14 contextMetadata
-        ]
-
-    @staticmethod
-    def get_authors(obj):
-        if not obj.created_by_user:
-            return
-        name = obj.created_by_user.username
-        if obj.created_by_user.last_name:
-            name = obj.created_by_user.last_name
-            if obj.created_by_user.first_name:
-                name = obj.created_by_user.first_name + " " + name
-        author = {
-            'name': name
-        }
-        if obj.created_by_user.email:
-            author['email'] = obj.created_by_user.email
-        return {
-            'authors': [author]
-        }
-
-    @staticmethod
-    def get_publisher(obj):
-        if not obj.owned_by_org:
-            return
-        return obj.owned_by_org.name
-
-    @staticmethod
-    def get_links(obj):
-        # NOTE: The structure specified in 7.1.1.1 differs from the example in Annex B, but the example seems more
-        # reasonable.
-        return {
-            'profiles': [{'href': 'http://www.opengis.net/spec/owc-geojson/1.0/req/core',
-                          'title': 'This file is compliant with version 1.0 of OGC Context'}]
-        }
-
-
 class MapContextLayerPropertiesSerializer(ModelSerializer):
     title = ModelField(model_field=MapContextLayer()._meta.get_field('name'))
     abstract = ModelField(model_field=MapContextLayer()._meta.get_field('title'))
+    offerings = SerializerMethodField()
     folder = SerializerMethodField()
 
     class Meta:
@@ -79,14 +22,36 @@ class MapContextLayerPropertiesSerializer(ModelSerializer):
             # TODO 'rights' # 7.1.2.7 rights
             # TODO 'date' # 7.1.2.9 temporalExtent
             # TODO 'links' # 7.1.2.10 preview + 7.1.2.11 contentDescription + 7.1.2.12 contentByRef + 7.1.2.15 resourceMetadata
-            # TODO 'offering' # 7.1.2.13 offering
+            'offerings',  # 7.1.2.13 offering
             # TODO 'active' # 7.1.2.14 active
             # TODO 'categories' # 7.1.2.16 keyword
             # TODO 'minScaleDenominator' # 7.1.2.17 minScaleDenominator
             # TODO 'maxScaleDenominator' # 7.1.2.18 maxScaleDenominator
             'folder',  # 7.1.2.19 folder
-            # TODO # 7.1.2.20 extension
+            # TODO '*', # 7.1.2.20 extension
         ]
+
+    @staticmethod
+    def __get_wms_offering(wms_layer):
+        return {
+            'code': 'http://www.opengis.net/spec/owc-geojson/1.0/req/wms',
+            'operations': [{
+                "code": "GetCapabilities",
+                "method": "GET",
+                "type": "application/xml",
+                "href": wms_layer.service.url + "service=WMS&request=GetCapabilities"
+            }, {
+                "code": "GetMap",
+                "method": "GET",
+                "type": "image/jpeg",  # TODO
+                "href": wms_layer.service.url + "service=WMS&version=1.3.0&request=GetMap"  # TODO full request url
+            }]
+        }
+
+    @staticmethod
+    def get_offerings(obj):
+        offerings = [MapContextLayerPropertiesSerializer.__get_wms_offering(obj.layer)]
+        return offerings
 
     @staticmethod
     def get_folder(obj):
@@ -119,6 +84,71 @@ class MapContextLayerSerializer(ModelSerializer):
             reverse('api:mapcontext-detail', args=[obj.map_context_id]) + "layer/" + str(obj.id) + "/")
 
 
+class MapContextPropertiesSerializer(ModelSerializer):
+    lang = CharField(default='de')
+    subtitle = ModelField(model_field=MapContext()._meta.get_field('abstract'))
+    updated = ModelField(model_field=MapContext()._meta.get_field('last_modified_at'))
+    authors = SerializerMethodField()
+    publisher = SerializerMethodField()
+    generator = SerializerMethodField()
+    links = SerializerMethodField()
+
+    class Meta:
+        model = MapContext
+        fields = [
+            'lang',  # 7.1.1.2 language
+            'title',  # 7.1.1.4 title
+            'subtitle',  # 7.1.1.5 abstract
+            'updated',  # 7.1.1.6 updateDate (TODO: get rid of fractional seconds)
+            'authors',  # 7.1.1.7 author
+            'publisher',  # 7.1.1.8 publisher
+            'generator',  # 7.1.1.9 creator / 7.1.7.1 creatorApplication
+            # TODO 'display',  # 7.1.9 DataType OWC:Creator/OWC:CreatorDisplay
+            # TODO 'rights', # 7.1.1.10 rights
+            # TODO 'categories', # 7.1.1.15 keyword
+            'links',  # 7.1.1.1 specReference + TODO: 7.1.1.14 contextMetadata
+        ]
+
+    @staticmethod
+    def get_authors(obj):
+        if not obj.created_by_user:
+            return
+        name = obj.created_by_user.username
+        if obj.created_by_user.last_name:
+            name = obj.created_by_user.last_name
+            if obj.created_by_user.first_name:
+                name = obj.created_by_user.first_name + " " + name
+        author = {
+            'name': name
+        }
+        if obj.created_by_user.email:
+            author['email'] = obj.created_by_user.email
+        return [author]
+
+    @staticmethod
+    def get_publisher(obj):
+        if not obj.owned_by_org:
+            return
+        return obj.owned_by_org.name
+
+    @staticmethod
+    def get_generator(obj):
+        return {
+            'title': 'MrMap',
+            'uri': 'https://mrmap-community.github.io/mrmap/develop/',  # TODO actual link
+            'version': '1.0'  # TODO actual version
+        }
+
+    @staticmethod
+    def get_links(obj):
+        # NOTE: The structure specified in 7.1.1.1 differs from the example in Annex B, but the example seems more
+        # reasonable.
+        return {
+            'profiles': [{'href': 'http://www.opengis.net/spec/owc-geojson/1.0/req/core',
+                          'title': 'This file is compliant with version 1.0 of OGC Context'}]
+        }
+
+
 class MapContextSerializer(HyperlinkedModelSerializer):
     type = CharField(default='FeatureCollection')
     id = HyperlinkedIdentityField(view_name="api:mapcontext-detail")
@@ -134,10 +164,10 @@ class MapContextSerializer(HyperlinkedModelSerializer):
             # TODO 'bbox',  # 7.1.1.11 areaOfInterest
             # TODO 'date',  # 7.1.1.12 timeIntervalOfInterest
             'features'  # 7.1.1.13 resource
-            # TODO # 7.1.1.16 extension
+            # TODO '*',  # 7.1.1.16 extension
         ]
 
     def get_features(self, obj):
-        queryset = MapContextLayer.objects.filter(map_context_id=obj.id)
+        queryset = MapContextLayer.objects.filter(map_context_id=obj.id).filter(layer__isnull=False)
         serializer = MapContextLayerSerializer(queryset, many=True, context=self.context)
         return serializer.data

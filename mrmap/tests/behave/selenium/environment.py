6aa48a0b9ef2311e3799eeb151bab317c3bdf2a9
joki <jonas.kiefer@live.com>
2021-11-05 10:11:49 +0100

    fixes pre commit checks

--- mrmap/tests/behave/selenium/environment.py
+++ mrmap/tests/behave/selenium/environment.py
@@ -1,10 +1,8 @@
-import time
 import behave_webdriver
 from datetime import datetime
 from selenium.webdriver.firefox.options import Options
-from datetime import datetime
 from behave.model_core import Status
-from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
+from selenium.common.exceptions import ElementClickInterceptedException
 
 
 def before_all(context):
@@ -24,19 +22,21 @@ def after_all(context):
 def after_step(context, step):
     if step.status == Status.failed:
         now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-        file_name = 'selenium-%(feature)s-%(step_number)d-%(date)s.png' % {"feature": context.scenario.feature.name.replace(" ", "_"), "step_number": step.line, "date": now}
+        file_name_data = {
+            "feature": context.scenario.feature.name.replace(" ", "_"),
+            "step_number": step.line, "date": now
+        }
+        file_name = 'selenium-%(feature)s-%(step_number)d-%(date)s.png' % file_name_data
         context.behave_driver.get_screenshot_as_file(file_name)
-        
+
         if isinstance(step.exception, ElementClickInterceptedException) and "is not clickable at point" in step.exception.msg:
             # add a circle to show where the click was done
             x, y = step.exception.msg.split("(")[1].split(")")[0].split(",")
             x = int(x)
             y = int(y)
-        
+
             from PIL import Image, ImageDraw
             with Image.open(file_name) as im:
                 draw = ImageDraw.Draw(im)
-                draw.ellipse(xy=(x-10, y-10, x+10, y+10), fill=(255, 0, 0), outline=(0, 0, 0))
+                draw.ellipse(xy=(x - 10, y - 10, x + 10, y + 10), fill=(255, 0, 0), outline=(0, 0, 0))
                 im.save(file_name)
-
-            

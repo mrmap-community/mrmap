fdafd473d6e069ea61dc7d8179ad6a9154a51191
Markus Schneider <mschneider@terrestris.de>
2021-11-10 16:18:09 +0100

    Frontend: Added table sorting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,15 @@
 import { Dashboard } from "./Components/Dashboard/Dashboard";
 import { ServiceList } from "./Components/Service/ServiceList/ServiceList";
-import { OpenAPIProvider } from "./Util/OpenAPIProvider";
 import { NavBar } from "./Components/NavBar/NavBar";
 import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
 import './App.css';
+import { OpenAPIProvider } from "./Hooks/OpenAPIProvider";
 
 
 export default function App() {
   return (
     <Router>
-      <OpenAPIProvider definition="https://localhost/api/schema/" axiosConfigDefaults={{ baseURL: "https://localhost" }}>
+      <OpenAPIProvider definition="http://localhost:8001/api/schema/" axiosConfigDefaults={{ baseURL: "http://localhost:8001" }}>
         <NavBar />
         <Routes>
           <Route path="/" element={<Dashboard/>}/>
--- frontend/src/Components/Service/ServiceList/ServiceList.tsx
+++ frontend/src/Components/Service/ServiceList/ServiceList.tsx
@@ -1,68 +1,89 @@
 import { useContext, useEffect, useState } from "react";
-import { Table, Card } from "antd";
-import { OpenAPIContext } from "../../../Util/OpenAPIProvider";
+import { Table, Card} from "antd";
+import { OpenAPIContext } from "../../../Hooks/OpenAPIProvider";
 
 export const ServiceList = () => {
 
     console.log("*** ServiceList");
 
-    const [loading, setLoading] = useState(false);
-    const [dataSource, setDataSource] = useState([]);
-    const [page, setPage] = useState(1);
-    const [pageSize, setPageSize] = useState(1);
-    const [total, setTotal] = useState(0);
+    // modified by data fetching
+    const [backendState, setBackendState] = useState({
+        loading: false,
+        dataSource: [],
+        total: 0
+    });
+
+    // modified by table interaction (page change, page size, sort order change, filter change)
+    const [tableState, setTableState] = useState<any>({
+        page: 1,
+        pageSize: 1,
+        ordering: undefined
+    });
+
     const [columns] = useState([{
         title: 'ID',
         dataIndex: 'id',
         key: 'id',
+        sorter: true,
     },
     {
         title: 'Title',
         dataIndex: 'title',
         key: 'title',
+        sorter: true,
     },
     {
         title: 'Abstract',
         dataIndex: 'abstract',
         key: 'abstract',
+        sorter: true
     },]);
 
     const { api } = useContext(OpenAPIContext);
 
     useEffect(() => {
+        console.log("*** fetching");
         async function fetchTableData() {
-            setLoading(true);
+            setBackendState({
+                ...backendState,
+                loading: true
+            });
             const client = await api.getClient();
             const res = await client.v1_registry_service_services_list({
-                page: page,
-                page_size: pageSize
+                page: tableState.page,
+                page_size: tableState.pageSize,
+                ordering: tableState.ordering
             });
-            if (total !== res.data.count) {
-                setTotal(res.data.count);
-            }
-            setDataSource(res.data.results);
-            setLoading(false);
+            setBackendState({
+                loading: false,
+                dataSource: res.data.results,
+                total: res.data.count
+            });
         }
         fetchTableData();
-    }, [page, pageSize, api, total]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [tableState, api]);
 
     function handleTableChange(pagination: any, filters: any, sorter: any) {
-        setPage(pagination.current);
-        setPageSize(pagination.pageSize);
+        setTableState({
+            page: pagination.current,
+            pageSize: pagination.pageSize,
+            ordering: sorter ? ((sorter.order === 'descend' ? '-' : '') + sorter.field) : undefined
+        });
     };
 
     return (
         <div className="mrmap-mapcontext-list">
             <Card title="Services" style={{ width: '100%' }}>
                 <Table
-                    dataSource={dataSource}
-                    rowKey={(record: any) => record.id}
+                    dataSource={backendState.dataSource}
+                    rowKey={(record:any) => record.id}
                     columns={columns}
-                    loading={loading}
+                    loading={backendState.loading}
                     pagination={{
-                        current: page,
-                        pageSize: pageSize,
-                        total: total
+                        current: tableState.page,
+                        pageSize: tableState.pageSize,
+                        total: backendState.total,
                     }}
                     onChange={handleTableChange}
                 />

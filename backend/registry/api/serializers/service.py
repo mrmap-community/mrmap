0445c1b96799d8efdb0b7f839cdc656bddba3b2c
jokiefer <jonas.kiefer@live.com>
2021-11-18 18:55:29 +0100

    cleanup

--- backend/registry/api/serializers/service.py
+++ backend/registry/api/serializers/service.py
@@ -1,11 +1,7 @@
-from django.core.exceptions import ObjectDoesNotExist
-from rest_framework.reverse import reverse
-from rest_framework.serializers import ModelSerializer, HyperlinkedRelatedField
-from rest_framework.fields import SerializerMethodField
+from rest_framework.serializers import ModelSerializer
 from extras.api.serializers import LinksSerializerMixin, ObjectAccessSerializer
 from registry.models.service import Layer, FeatureType, Service, OperationUrl, ServiceType
 from registry.models.metadata import Keyword
-from registry.enums.service import OGCServiceEnum
 from rest_framework_json_api.relations import ResourceRelatedField
 
 
@@ -17,16 +13,6 @@ class OperationsUrlSerializer(ModelSerializer):
 
 
 class LayerSerializer(ObjectAccessSerializer):
-    # TODO: extreme slow lookup... Present a link to the dataset_metadata entpoint filtered by the given layer
-    # dataset_metadata = HyperlinkedRelatedField(
-    #     many=True,
-    #     read_only=True,
-    #     view_name='api:dataset_metadata-detail'
-    # )
-    # service = HyperlinkedRelatedField(
-    #     read_only=True,
-    #     view_name='api:service-detail'
-    # )
 
     class Meta:
         model = Layer
@@ -34,12 +20,6 @@ class LayerSerializer(ObjectAccessSerializer):
 
 
 class FeatureTypeSerializer(ObjectAccessSerializer):
-    # FIXME: extreme slow lookup... Present a link to the dataset_metadata entpoint filtered by the given ft
-    # dataset_metadata = HyperlinkedRelatedField(
-    #     many=True,
-    #     read_only=True,
-    #     view_name='api:dataset_metadata-detail'
-    # )
 
     class Meta:
         model = FeatureType
@@ -61,7 +41,6 @@ class ServiceTypeSerializer(ModelSerializer):
 
 
 class ServiceSerializer(LinksSerializerMixin, ObjectAccessSerializer):
-    #type = ServiceTypeSerializer(source='service_type')
 
     class Meta:
         model = Service
@@ -82,45 +61,3 @@ class ServiceSerializer(LinksSerializerMixin, ObjectAccessSerializer):
         related_link_url_kwarg='parent_lookup_service',
         self_link_view_name='registry:service-relationships',
     )
-
-
-    # def get_links(self, obj):
-    #     links = []
-    #     from django.db.models.fields.related_descriptors import ReverseManyToOneDescriptor
-
-    #     for field in self.links_fields:
-    #         if isinstance(obj.__class__.layers, ReverseManyToOneDescriptor):
-    #             query_param = f"{obj.__class__.layers.rel.remote_field.name}__{obj.__class__.layers.rel.field_name}"
-    #             query = f"?{query_param}={getattr(obj, obj.__class__.layers.rel.field_name)}"
-    #             reverse_name = f"api:{obj.__class__.layers.field.opts.model_name}-list"
-    #             path = f"{reverse(reverse_name)}"
-    #             absolute_uri = self.context['request'].build_absolute_uri(path) + query
-    #             links.append({
-    #                 "rel": "child",
-    #                 "kind": "collection",
-    #                 "name": "keywords",
-    #                 "count": "TODO",
-    #                 "href": absolute_uri
-
-    #             })
-    #     return links
-
-    # def get_layers(self, obj):
-    #     return self.context['request'].build_absolute_uri(f"{reverse('api:layer-list')}?service__id={obj.pk}")
-
-    # def get_feature_types(self, obj) -> FeatureTypeSerializer:
-    #     queryset = FeatureType.objects.none()
-
-    #     if obj.is_service_type(OGCServiceEnum.WFS):
-    #         queryset = obj.featuretypes.all().prefetch_related('keywords')
-    #     return FeatureTypeSerializer(queryset, many=True, context=self.context).data
-
-    # @staticmethod
-    # def get_keywords(obj) -> KeywordSerializer:
-    #     try:
-    #         keywords = obj.keywords
-    #     except ObjectDoesNotExist:
-    #         keywords = None
-    #     if keywords:
-    #         return KeywordSerializer(keywords, many=True).data
-    #     return None
--- backend/registry/api/views/service.py
+++ backend/registry/api/views/service.py
@@ -1,11 +1,4 @@
-from django_filters import rest_framework as api_filters
-from rest_framework.filters import OrderingFilter
-from rest_framework.permissions import DjangoObjectPermissions
-
 from extras.api import viewsets
-from extras.api.pagination import StandardResultsSetPagination
-from extras.api.viewsets import ModelViewSetWithPermissionChecker
-from registry.api.filters.service import ServiceApiFilter, FeatureTypeApiFilter, LayerApiFilter
 from registry.api.serializers.service import ServiceSerializer, FeatureTypeSerializer, LayerSerializer
 from registry.models import Service, Layer, FeatureType
 from rest_framework_extensions.mixins import NestedViewSetMixin
@@ -19,27 +12,7 @@ class ServiceRelationshipView(RelationshipView):
 class ServiceViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
-    #filterset_class = ServiceApiFilter
-    #filter_backends = [api_filters.DjangoFilterBackend, OrderingFilter]
-    #ordering_fields = ['title', 'abstract', 'id']
-    #pagination_class = StandardResultsSetPagination
-    # permission_classes = [DjangoObjectPermissions]
-
-    # serializers = {
-    #     'default': ServiceSerializer
-    # }
-
-    # def get_serializer_class(self):
-    #     return self.serializers.get(self.action, self.serializers['default'])
-
-    # TODO: add to mixin (not needed for now. Will come in handy when we need to start overwriting methods)
-    # def get_serialized_status_ok_response(self, _queryset, _many=True, _status=status.HTTP_200_OK):
-    #    serializer = self.get_serializer(_queryset, many=_many)
-    #    return Response(data=serializer.data, status=_status)
-
-    # def get_queryset(self, *args, **kwargs):
-        # return super().get_queryset().prefetch_related('keywords').select_related('owned_by_org', 'service_type')
-
+    
 
 class LayerViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = Layer.objects.all()
@@ -59,43 +32,19 @@ class LayerViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
         return queryset
 
 
-    #filterset_class = LayerApiFilter
-    #filter_backends = [api_filters.DjangoFilterBackend, OrderingFilter]
-    #ordering_fields = ['title', 'abstract', 'id']
-    #pagination_class = StandardResultsSetPagination
-    # permission_classes = [DjangoObjectPermissions]
-
-    # serializers = {
-    #     'default': LayerSerializer
-    # }
-
-    # def get_serializer_class(self):
-    #     return self.serializers.get(self.action, self.serializers['default'])
-
-    # def get_queryset(self, *args, **kwargs):
-    #     return super().get_queryset().prefetch_related('keywords').select_related('owned_by_org', 'service')
-
-
-class FeatureTypeViewSet(NestedViewSetMixin, ModelViewSetWithPermissionChecker):
+class FeatureTypeViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = FeatureType.objects.all()
     serializer_class = FeatureTypeSerializer
-    # filterset_class = FeatureTypeApiFilter
-    # filter_backends = [api_filters.DjangoFilterBackend, OrderingFilter]
-    # ordering_fields = ['title', 'abstract', 'id']
-    # pagination_class = StandardResultsSetPagination
-    # permission_classes = [DjangoObjectPermissions]
 
-    # serializers = {
-    #     'default': FeatureTypeSerializer
-    # }
+    def get_queryset(self):
+        queryset = super(FeatureTypeViewSet, self).get_queryset()
 
-    # def get_serializer_class(self):
-    #     return self.serializers.get(self.action, self.serializers['default'])
+        # if this viewset is accessed via the 'service-layers-list' route,
+        # it wll have been passed the `service_pk` kwarg and the queryset
+        # needs to be filtered accordingly; if it was accessed via the
+        # unnested '/services' route, the queryset should include all layers
+        if 'service_pk' in self.kwargs:
+            service_pk = self.kwargs['service_pk']
+            queryset = queryset.filter(service__pk=service_pk)
 
-    # # TODO: add to mixin (not needed for now. Will come in handy when we need to start overwriting methods)
-    # # def get_serialized_status_ok_response(self, _queryset, _many=True, _status=status.HTTP_200_OK):
-    # #    serializer = self.get_serializer(_queryset, many=_many)
-    # #    return Response(data=serializer.data, status=_status)
-
-    # def get_queryset(self, *args, **kwargs):
-    #     return super().get_queryset().prefetch_related('keywords').select_related('owned_by_org', 'service')
+        return queryset

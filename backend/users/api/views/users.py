4d9f30bface4eb3f8fe7829caa33a64447d90d25
jokiefer <jonas.kiefer@live.com>
2021-11-23 09:33:49 +0100

    adds tags to categorize api endpoints with autoschema

--- backend/users/api/views/users.py
+++ backend/users/api/views/users.py
@@ -1,3 +1,4 @@
+from rest_framework_json_api.schemas.openapi import AutoSchema
 from users.api.serializers.users import MrMapUserSerializer
 from users.models.users import MrMapUser
 from rest_framework_extensions.mixins import NestedViewSetMixin
@@ -5,6 +6,9 @@ from rest_framework_json_api.views import ModelViewSet
 
 
 class MrMapUserViewSet(NestedViewSetMixin, ModelViewSet):
+    schema = AutoSchema(
+        tags=['Users'],
+    )
     queryset = MrMapUser.objects.all()
     serializer_class = MrMapUserSerializer
     prefetch_for_includes = {

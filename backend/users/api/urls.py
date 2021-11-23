8dc975f57563a9f2a67c4e8332c5a12b7545e3be
jokiefer <jonas.kiefer@live.com>
2021-11-23 11:59:25 +0100

    added users/{id}/organizations/ endpoint

--- backend/users/api/urls.py
+++ backend/users/api/urls.py
@@ -6,9 +6,10 @@ app_name = 'users'
 
 nested_api_router = ExtendedSimpleRouter()
 (
-    nested_api_router.register(r'users', user_views.MrMapUserViewSet, basename='mrmapuser'),
-    nested_api_router.register(r'groups', group_views.GroupViewSet, basename='organization'), 
-    nested_api_router.register(r'organizations', group_views.OrganizationViewSet, basename='organization'), 
+    nested_api_router.register(r'users', user_views.MrMapUserViewSet, basename='mrmapuser')
+                     .register(r'organizations', group_views.OrganizationViewSet, basename='organization', parents_query_lookups=['user']),
+    nested_api_router.register(r'groups', group_views.GroupViewSet, basename='organization'),
+    nested_api_router.register(r'organizations', group_views.OrganizationViewSet, basename='organization'),
 )
 
 urlpatterns = nested_api_router.urls

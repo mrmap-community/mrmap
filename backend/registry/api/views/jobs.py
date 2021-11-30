3589d5b1e721e9ee635a12568bd5d9a598319de6
jokiefer <jonas.kiefer@live.com>
2021-11-30 09:27:55 +0100

    * remove unused files * add filter_fields to task-result endpoint

--- backend/registry/api/views/jobs.py
+++ backend/registry/api/views/jobs.py
@@ -1,15 +1,21 @@
-from registry.api.serializers.jobs import TaskResultSerializer
-from rest_framework_json_api.views import ReadOnlyModelViewSet
-from django_celery_results.models import TaskResult
-from celery import states
-from rest_framework.response import Response
-from rest_framework import status
 import json
+
+from celery import states
+from django_celery_results.models import TaskResult
+from registry.api.serializers.jobs import TaskResultSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework_json_api.views import ReadOnlyModelViewSet
 
 
 class TaskResultReadOnlyViewSet(ReadOnlyModelViewSet):
     queryset = TaskResult.objects.all()
     serializer_class = TaskResultSerializer
+    filter_fields = {
+        "task_name": ["exact", "icontains", "contains"],
+        "status": ["exact"],
+        "date_created": ["exact", "gt", "lt", "range"],
+    }
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -18,8 +24,16 @@ class TaskResultReadOnlyViewSet(ReadOnlyModelViewSet):
             # followed the jsonapi recommendation for async processing
 
             # https://jsonapi.org/recommendations/#asynchronous-processing
+            # FIXME: rethink about the data schema of the result field... It could be possible that there is no api_endpoint which represents the concrete result as json:api resource
             result = json.loads(instance.result)
-            return Response(status=status.HTTP_303_SEE_OTHER, headers={'Location': str(self.request.build_absolute_uri(result.get("api_enpoint")))})
+            return Response(
+                status=status.HTTP_303_SEE_OTHER,
+                headers={
+                    "Location": str(
+                        self.request.build_absolute_uri(result.get("api_enpoint"))
+                    )
+                },
+            )
         else:
             serializer = self.get_serializer(instance)
             return Response(serializer.data)

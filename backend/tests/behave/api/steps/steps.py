559e07edf6fd9be1dc24ed15d506d836051060db
jokiefer <jonas.kiefer@live.com>
2021-12-07 10:09:35 +0100

    Update requirements.dev.txt and steps.py

--- backend/tests/behave/api/steps/steps.py
+++ backend/tests/behave/api/steps/steps.py
@@ -1,18 +1,13 @@
 from accounts.models import User
 from behave import given, step, then
-from django.apps import apps
 from guardian.shortcuts import assign_perm
+from model_bakery import baker
 from rest_framework.authtoken.models import Token
 
 
-@step('there are set of Model {model} in Database')
-def step_impl(context, model):
-    app_label, model_name = model.split('.')
-    Model = apps.get_model(app_label=app_label, model_name=model_name)
-    objs = []
-    for row in context.table:
-        objs.append(Model(**dict(row.as_dict())))
-    Model.objects.bulk_create(objs)
+@step('there are set of {quantity} Model {model} in Database')
+def step_impl(context, quantity, model):
+    baker.make(model, _quantity=quantity)
 
 
 @step('there are set of Users in Database')

28658bd1ec2a77cc2d4713fbc1c49b160bada7d5
Markus Schneider <mschneider@terrestris.de>
2021-12-08 16:14:07 +0100

    Frontend: Enable customization of actions in RepoTable.

--- frontend/src/Components/Shared/Table/RepoTable.tsx
+++ frontend/src/Components/Shared/Table/RepoTable.tsx
@@ -1,10 +1,10 @@
 import '@ant-design/pro-table/dist/table.css';
 
 import { PlusOutlined } from '@ant-design/icons';
-import ProTable, { ProColumnType } from '@ant-design/pro-table';
+import ProTable, { ActionType, ProColumnType } from '@ant-design/pro-table';
 import { Button, Card, Modal, notification, Space } from 'antd';
 import { SortOrder } from 'antd/lib/table/interface';
-import React, { ReactElement, useEffect, useRef, useState } from 'react';
+import React, { MutableRefObject, ReactElement, useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router';
 
 import JsonApiRepo from '../../../Repos/JsonApiRepo';
@@ -12,10 +12,15 @@ import { augmentColumnWithJsonSchema } from './TableHelper';
 
 interface RepoTableProps {
     repo: JsonApiRepo
+    actionRef?: MutableRefObject<RepoActionType> | ((actions: RepoActionType) => void)
     addRecord?: string
     editRecord?: boolean,
     onEditRecord?: (recordId: number | string) => void,
-    columnHints?: ProColumnType[];
+    columnHints?: ProColumnType[],
+}
+
+export type RepoActionType = ActionType & {
+  deleteRecord: (row:any) => void
 }
 
 function deriveColumns (resourceSchema: any, columnHints: ProColumnType[] | undefined): ProColumnType[] {
@@ -37,69 +42,66 @@ function deriveColumns (resourceSchema: any, columnHints: ProColumnType[] | unde
       dataIndex: propName
     };
     columns[propName] = augmentColumnWithJsonSchema(columnHint, prop);
-    // const column: any = {
-    //   title: prop.title || propName,
-    //   dataIndex: propName,
-    //   key: propName,
-    //   valueType: undefined
-    // };
-    // prop.isFlat = prop.type === 'string';
-    // if (prop.isFlat) {
-    //   column.sorter = true;
-    // }
-    // columns.push(column);
   }
   return Object.values(columns);
 }
 
 export const RepoTable = ({
   repo,
+  actionRef = undefined,
   addRecord,
   editRecord = false,
   onEditRecord = () => undefined,
   columnHints = undefined
 }: RepoTableProps): ReactElement => {
   const navigate = useNavigate();
-  const actionRef = useRef();
   const [columns, setColumns] = useState<any>([]);
+
+  const actions = useRef<RepoActionType>();
+  const setActions = (proTableActions: ActionType) => {
+    actions.current = {
+      ...proTableActions,
+      deleteRecord: (row:any) => {
+        async function deleteFromRepo () {
+          await repo.delete(row.id);
+          notification.success({
+            message: 'Record deleted',
+            description: `Record with id ${row.id} has been deleted succesfully`
+          });
+        }
+        const modal = Modal.confirm({
+          title: 'Delete record',
+          content: `Do you want to delete the record with id ${row.id}?`,
+          onOk: () => {
+            modal.update(prevConfig => ({
+              ...prevConfig,
+              confirmLoading: true
+            }));
+            deleteFromRepo();
+            proTableActions.reload();
+          }
+        });
+      }
+    };
+    if (typeof actionRef === 'function') {
+      actionRef(actions.current);
+    } else if (actionRef) {
+      actionRef.current = actions.current;
+    }
+  };
 
   // build columns from schema (and add delete action)
   useEffect(() => {
-    const onDeleteRecord = (record: any) => {
-      async function deleteRecord () {
-        await repo.delete(record.id);
-        notification.success({
-          message: 'Record deleted',
-          description: `Record with id ${record.id} has been deleted succesfully`
-        });
-      }
-      const modal = Modal.confirm({
-        title: 'Delete record',
-        content: `Do you want to delete the record with id ${record.id}?`,
-        onOk: () => {
-          modal.update(prevConfig => ({
-            ...prevConfig,
-            confirmLoading: true
-          }));
-          deleteRecord();
-          if (actionRef.current) {
-            (actionRef.current as any).reload();
-          }
-        }
-      });
-    };
-
     async function buildColumns () {
       const schema = await repo.getSchema();
-      console.log('schema', schema);
       const columns = deriveColumns(schema, columnHints);
-      columns.push({
-        title: 'Actions',
-        key: 'actions',
-        valueType: 'option',
-        render: (text: any, record: any) => {
-          const boundOnDeleteRecord = onDeleteRecord.bind(null, record);
-          return (
+      if (!columns.some(column => column.key === 'actions')) {
+        columns.push({
+          title: 'Actions',
+          key: 'actions',
+          valueType: 'option',
+          render: (text: any, record: any) => {
+            return (
             <>
               <Space size='middle'>
               {editRecord && (
@@ -113,15 +115,16 @@ export const RepoTable = ({
                 <Button
                   danger
                   size='small'
-                  onClick={boundOnDeleteRecord}
+                  onClick={() => actions.current?.deleteRecord(record)}
                 >
                   Delete
                 </Button>
               </Space>
             </>
-          );
-        }
-      });
+            );
+          }
+        });
+      }
       setColumns(columns);
     }
     buildColumns();
@@ -183,7 +186,7 @@ export const RepoTable = ({
         columns={columns}
         scroll={{ x: true }}
         headerTitle={'Records'}
-        actionRef={actionRef}
+        actionRef={setActions}
         toolBarRender={() => [
           <Button
             type='primary'

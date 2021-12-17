9ab0754e40a26b5d50ec33d34afcd648e426ef58
jokiefer <jonas.kiefer@live.com>
2021-12-17 14:38:14 +0100

    change serialize data type of task meta and result property to json

--- backend/notify/serializers.py
+++ backend/notify/serializers.py
@@ -1,11 +1,15 @@
+import json
+
 from django_celery_results.models import TaskResult
+from rest_framework.fields import SerializerMethodField
 from rest_framework.relations import HyperlinkedIdentityField
 from rest_framework_json_api.serializers import CharField, ModelSerializer
 
 
 class TaskResultSerializer(ModelSerializer):
 
-    task_meta = CharField(source='meta')
+    task_meta = SerializerMethodField()
+    result = SerializerMethodField()
 
     url = HyperlinkedIdentityField(
         view_name='notify:taskresult-detail',
@@ -16,3 +20,9 @@ class TaskResultSerializer(ModelSerializer):
         model = TaskResult
         # meta field clashes with json:api meta field. We use alternate naming. See task_meta above.
         exclude = ("meta", )
+
+    def get_task_meta(self, obj):
+        return json.loads(obj.meta)
+
+    def get_result(self, obj):
+        return json.loads(obj.result)

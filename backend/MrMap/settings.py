19147587a3ec045f66bc435dd908b3bf3ffcb9a7
jokiefer <jonas.kiefer@live.com>
2021-12-28 15:22:35 +0100

    Update settings.py

--- backend/MrMap/settings.py
+++ backend/MrMap/settings.py
@@ -88,10 +88,7 @@ TEMPLATES = [
 ]
 
 if DEBUG:
-    import os  # only if you haven't already imported this
-    import socket  # only if you haven't already imported this
-    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-    INTERNAL_IPS = [ip[:-1] + '1' for ip in ips] + ['127.0.0.1', '10.0.2.2']
+
     INSTALLED_APPS.append(
         "debug_toolbar",
     )

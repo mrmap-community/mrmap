905a3e19bcecf558b0e6d337348f167cf82c5eb6
Unraveler <rui.nunes1985@gmail.com>
2022-01-17 15:11:38 +0100

    fixed url on LayerRepo

--- frontend/src/Repos/LayerRepo.ts
+++ frontend/src/Repos/LayerRepo.ts
@@ -18,6 +18,14 @@ const getServiceType = (url:string): string => {
   }
   return '';
 };
+
+const getServiceUrl = (includedServices: any[], operation:string) => {
+  const getMapService = includedServices.find(service => service.attributes.operation === operation);
+  if(getMapService) {
+
+    return getMapService.attributes.url;
+  }
+};
 class LayerRepo extends JsonApiRepo {
   constructor () {
     super('/api/v1/registry/layers/', 'WMS-Ebenen');
@@ -44,7 +52,7 @@ class LayerRepo extends JsonApiRepo {
         ...o.attributes,
         WMSParams: {
           layer: o.attributes.identifier,
-          url: res.data.included && res.data.included.length > 0 && res.data.included[0].attributes.service_url,
+          url: res.data.included && res.data.included.length > 0 && getServiceUrl(res.data.included, 'GetMap'),
           version: res.data.included && res.data.included.length > 0 && res.data.included[0].attributes.version,
           serviceType: res.data.included && res.data.included.length > 0 && 
             getServiceType(res.data.included[0].attributes.service_url)
@@ -93,7 +101,7 @@ class LayerRepo extends JsonApiRepo {
         WMSParams: {
           bbox: extent,
           layer: res.data.data.attributes.identifier,
-          url: res.data.included && res.data.included.length > 0 && res.data.included[0].attributes.service_url,
+          url: res.data.included && res.data.included.length > 0  && getServiceUrl(res.data.included, 'GetMap'),
           version: res.data.included && res.data.included.length > 0 && res.data.included[0].attributes.version,
           serviceType: res.data.included && res.data.included.length > 0 && 
             getServiceType(res.data.included[0].attributes.service_url),

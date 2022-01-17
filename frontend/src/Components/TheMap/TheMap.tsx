a395702f993601fe9d60f14bf3ecfc95012703fa
Unraveler <rui.nunes1985@gmail.com>
2022-01-18 00:44:33 +0100

    Added popup info buble

--- frontend/src/Components/TheMap/TheMap.tsx
+++ frontend/src/Components/TheMap/TheMap.tsx
@@ -1,12 +1,22 @@
 
+import { CloseOutlined } from '@ant-design/icons';
 import { MapComponent, useMap } from '@terrestris/react-geo';
+import Button from 'antd/lib/button';
+import { EventsKey as OlEventsKey } from 'ol/events';
 import LayerGroup from 'ol/layer/Group';
 import OlLayerTile from 'ol/layer/Tile';
 import OlMap from 'ol/Map';
+import { unByKey } from 'ol/Observable';
+import Overlay from 'ol/Overlay';
 import OlSourceOsm from 'ol/source/OSM';
 import OlView from 'ol/View';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { LayerUtils } from '../../Utils/LayerUtils';
+import { MapUtils } from '../../Utils/MapUtils';
 import './TheMap.css';
+
+const layerUtils = new LayerUtils();
+const mapUtils = new MapUtils();
 
 // TODO: Should be in a separate component or helper
 const layerGroup = new LayerGroup({
@@ -31,18 +41,93 @@ export const olMap = new OlMap({
   layers: [layerGroup]
 });
 
+
+
+const olListenerKeys: (OlEventsKey[]) = [];
+
 export const TheMap = (): JSX.Element => {
   const map = useMap();
 
-  useEffect(() => {
+  const [coordinates, setCoordinates] = useState<any>();
+  
+  const registerMapClickListener = (_olMap: OlMap, _overlay?: Overlay) => {
+    const getFeatureAttributesClickEventKey = _olMap
+      .on('singleclick', (event) => layerUtils.getFeatureAttributes(_olMap, event));
+    if(_overlay) {
+      const onShowInfoPopUpOnCoordinateClickListener = _olMap
+        .on('singleclick', async (event) => {
+          const coordinate = event.coordinate;
+          const featureInfo = layerUtils.getFeatureAttributes(_olMap, event);
+          _overlay.setPosition(coordinate);
+          try {
+            const result = await featureInfo;
+            if(result) {
+              // Object.keys(result).forEach(((key:any) => {
+
+              // })
+            }
+          } catch(error) {
+            //@ts-ignore
+            throw new Error(error);
+          }
+          
+        });
+      olListenerKeys.push(onShowInfoPopUpOnCoordinateClickListener);
+    }
+    olListenerKeys.push(getFeatureAttributesClickEventKey);
+  };
+
+  useEffect(() => {    
+    const close = document.getElementById('popup-close');
+    
+  
+    const infoPopUpBubble: Overlay = new Overlay({
+      //@ts-ignore
+      element: document.getElementById('popup'),
+      autoPan: {
+        animation: {
+          duration: 250,
+        },
+      },
+    });
+    
+    if(close) {
+      close.onclick = function () {
+        infoPopUpBubble.setPosition(undefined);
+        close.blur();
+        return false;
+      };
+    }
+    
+    // const overlay = infoPopUpBubble(container);
     map.setTarget('the-map');
+    registerMapClickListener(map, infoPopUpBubble);
+    map.addOverlay(infoPopUpBubble);
+    return () => {
+      // unregister the listener
+      unByKey(olListenerKeys);
+      map.removeOverlay(infoPopUpBubble);
+    };
   }, [map]);
-  
-  
+
   return (
-    <MapComponent
-      id='the-map'
-      map={map}
-    />
+    <>
+      <MapComponent
+        id='the-map'
+        map={map}
+      />
+      <div id='popup' className='ol-popup'>
+        <Button
+          id='popup-close'
+          type='link'
+          icon={<CloseOutlined />}
+          size='small'
+        />
+  
+        <div id='popup-content'>
+          {coordinates}
+        </div>
+      </div>
+    </>
   );
 };

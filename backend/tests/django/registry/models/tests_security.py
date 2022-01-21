034397a050a53c6abc9a3e601cc73c252d6c95db
jokiefer <jonas.kiefer@live.com>
2022-01-21 18:42:20 +0100

    * fixes testcases in test_security

--- backend/tests/django/registry/models/tests_security.py
+++ backend/tests/django/registry/models/tests_security.py
@@ -1,7 +1,8 @@
 from django.contrib.gis.geos import GEOSGeometry
-from django.db import IntegrityError
+from django.db.utils import IntegrityError
 from django.test import TestCase
 from registry.models.security import AllowedWebMapServiceOperation
+from registry.models.service import WebMapService
 
 
 class AllowedWebMapServiceOperationModelTest(TestCase):
@@ -9,9 +10,15 @@ class AllowedWebMapServiceOperationModelTest(TestCase):
     fixtures = ["test_wms.json"]
 
     def test_save_without_geometry(self):
-        self.assertRaises(None, AllowedWebMapServiceOperation.objects.create(
-            secured_service='cd16cc1f-3abb-4625-bb96-fbe80dbe23e3'))
+        allowed_wms_operation = AllowedWebMapServiceOperation.objects.create(
+            secured_service=WebMapService.objects.get(pk='cd16cc1f-3abb-4625-bb96-fbe80dbe23e3'))
+        self.assertIsInstance(obj=allowed_wms_operation,
+                              cls=AllowedWebMapServiceOperation)
 
     def test_save_with_empty_geometry(self):
-        self.assertRaises(IntegrityError, AllowedWebMapServiceOperation.objects.create(
-            secured_service='cd16cc1f-3abb-4625-bb96-fbe80dbe23e3', allowed_area=GEOSGeometry("POLYGON EMPTY")))
+        self.assertRaises(
+            IntegrityError,
+            AllowedWebMapServiceOperation.objects.create,
+            secured_service=WebMapService.objects.get(
+                pk='cd16cc1f-3abb-4625-bb96-fbe80dbe23e3'),
+            allowed_area=GEOSGeometry("MULTIPOLYGON EMPTY"))

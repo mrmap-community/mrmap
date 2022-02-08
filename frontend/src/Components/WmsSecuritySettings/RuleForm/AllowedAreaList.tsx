44d91ab1ee70e1f1dd9e21667e81896bad006f6e
Markus Schneider <mschneider@terrestris.de>
2022-02-08 20:44:04 +0100

    AllowedAreaList: Auto-synchronize with layers in map, implemented zooming.

--- frontend/src/Components/WmsSecuritySettings/RuleForm/AllowedAreaList.tsx
+++ frontend/src/Components/WmsSecuritySettings/RuleForm/AllowedAreaList.tsx
@@ -1,20 +1,40 @@
 import ProTable from '@ant-design/pro-table';
-import { default as React, ReactElement } from 'react';
-
+import { useMap } from '@terrestris/react-geo';
+import { DigitizeUtil } from '@terrestris/react-geo/dist/Util/DigitizeUtil';
+import BaseEvent from 'ol/events/Event';
+import { default as Geometry, default as OlGeometry } from 'ol/geom/Geometry';
+import VectorSource from 'ol/source/Vector';
+import { default as React, ReactElement, useEffect, useState } from 'react';
 
 export const AllowedAreaList = (): ReactElement => {
 
-  const dataSource = [
-    {
-      key: '1',
-      name: 'Polygon 1',
-    },
-    {
-      key: '2',
-      name: 'Polygon 2',
-    },
-  ];
-  
+  const map = useMap();
+  const [dataSource, setDataSource] = useState([] as any[]);
+
+  const onAreaSourceChanged = (source: VectorSource<Geometry>, event: BaseEvent|Event) => {
+    const newDataSource:any [] = [];
+    let i = 1;
+    source.getFeatures().forEach ( (feature) => {
+      newDataSource.push ({
+        key: String (i),
+        name: `Polygon ${i}`,
+        feature: feature
+      });
+      i++;
+    });
+    setDataSource(newDataSource);
+  };
+
+  useEffect ( () => {
+    const layer = DigitizeUtil.getDigitizeLayer(map);
+    const source = layer.getSource();
+    const handler = onAreaSourceChanged.bind(null, source);
+    source.on ('change', handler);
+    return ( () => {
+      source.un ('change', handler);
+    });
+  },[map]);
+
   const columns = [
     {
       title: 'Name',
@@ -25,16 +45,26 @@ export const AllowedAreaList = (): ReactElement => {
 
   return (
     <>
-      <ProTable
-        bordered={true}
-        cardBordered={true}
-        columns={columns}
-        dataSource={dataSource}
-        showHeader={false}
-        pagination={false}
-        search={false}
-        options={false}
-        toolBarRender={false}
-      />
+      {
+        <ProTable
+          bordered={true}
+          cardBordered={true}
+          columns={columns}
+          dataSource={dataSource}
+          showHeader={false}
+          pagination={false}
+          search={false}
+          options={false}
+          toolBarRender={false}
+          onRow={(record, rowIndex) => {
+            return {
+              onClick: event => {
+                const geom: OlGeometry = record.feature.getGeometry();
+                map.getView().fit(geom.getExtent());
+              }
+            };
+          }}          
+        />
+      }
     </>);
 };

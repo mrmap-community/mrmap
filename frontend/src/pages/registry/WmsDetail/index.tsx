d99cf6d3bf6481afc716809dbdb5498aee91e2a0
jokiefer <jonas.kiefer@live.com>
2022-05-02 20:08:48 +0200

    * implements active switch * changes Tree component to Collapse

--- frontend/src/pages/registry/WmsDetail/index.tsx
+++ frontend/src/pages/registry/WmsDetail/index.tsx
@@ -1,11 +1,18 @@
 import type { JsonApiDocument, JsonApiPrimaryData } from '@/utils/jsonapi';
+import { buildJsonApiPayload } from '@/utils/jsonapi';
+import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
 import { PageContainer } from '@ant-design/pro-layout';
-import { Tree } from 'antd';
-import type { ReactElement } from 'react';
+import { Collapse, Switch, Tooltip, Tree } from 'antd';
+import type { ReactElement, ReactNode } from 'react';
 import { useEffect, useState } from 'react';
 import { useOperationMethod } from 'react-openapi-client/useOperationMethod';
 import { useParams } from 'react-router';
+
+const { Panel } = Collapse;
+
 const { DirectoryTree } = Tree;
+
+
 interface Node {
     key: any;
     raw: JsonApiPrimaryData;
@@ -65,32 +72,45 @@ const transformTreeData = (wms: JsonApiDocument): Node[] => {
 
 
 
+
 const WmsDetails = (): ReactElement => {
     const { id } = useParams<{ id: string }>();
     const [
         getWebMapService, 
         { loading: getWMSLoading, response: getWMSResponse }
     ] = useOperationMethod('getWebMapService');
+    const [
+        updateLayer,
+        { loading: updateLayerLoading, response: updateLayerResponse}
+    ] = useOperationMethod('updateLayer');
     const [ treeData, setTreeData] = useState<Node[]>();
 
+    const getWebMapServiceParams = [
+        {
+            in: 'path',
+            name: 'id',
+            value: String(id),
+        },
+        {
+            in: 'query',
+            name: 'include',
+            value: 'layers',
+        },
+        {
+            in: 'query',
+            name: 'fields[Layer]',
+            value: 'string_representation,lft,rght,level,is_active'
+        }
+    ];
+    
     /**
      * @description Initial hook to receive the service
      */
     useEffect(() => {
-        getWebMapService(
-        [
-            {
-              in: 'path',
-              name: 'id',
-              value: String(id),
-            },
-            {
-              in: 'query',
-              name: 'include',
-              value: 'layers',
-            },
-          ]);
-    }, [getWebMapService, id]);
+        if (!getWMSLoading){
+            getWebMapService(getWebMapServiceParams);
+        }
+    }, [updateLayerResponse]);
 
     /**
      * @description Transform jsonapi response to needed tree data
@@ -100,17 +120,83 @@ const WmsDetails = (): ReactElement => {
             const newTreeData = transformTreeData(getWMSResponse.data)
             setTreeData(newTreeData);
         }
-    }, [getWMSResponse])
+    }, [getWMSResponse]);
+
+
+    const genExtra = (node: Node): ReactNode => {
+        const isActive = node?.raw?.attributes?.isActive;
+        return (
+            <>
+                <Tooltip
+                title={
+                    isActive
+                    ? 'deactivate'
+                    : 'activate'
+                }
+                >
+                    <Switch
+                        checkedChildren={<CheckOutlined />}
+                        unCheckedChildren={<CloseOutlined />}
+                        defaultChecked={isActive}
+                        onClick={
+                            (checked, event) => {
+                                // If you don't want click extra trigger collapse, you can prevent this:
+                                event.stopPropagation();
+                                updateLayer(
+                                    [{
+                                        in: 'path',
+                                        name: 'id',
+                                        value: String(node.key),
+                                    }], 
+                                    buildJsonApiPayload(node.raw.type, node.key, {isActive: checked })
+                                )
+                            }
+                        }
+                        loading={getWMSLoading || updateLayerLoading}
+                        
+                        
+                    />
+                </Tooltip>
+            </>
+        );
+    };
+    
+    const getCollapseableTree = (node: Node) => {
+        if (node.children){
+            return (
+                <Collapse >
+                    <Panel header={node.title} key={node.key} extra={genExtra(node)}>
+                    {
+                        node.children.map(
+                            (child: Node) => {
+                                return (getCollapseableTree(child))
+                            }
+                        )
+                    }
+                    </Panel>
+                </Collapse>
+            );
+        } else {
+            return (
+                <Collapse >
+                    <Panel header={node.title} key={node.key} extra={genExtra(node)}/>
+                </Collapse>
+            )
+        }
+    };
 
     return (
         <PageContainer>
-            <DirectoryTree
+            {/* <DirectoryTree
                 multiple
                 defaultExpandAll
                 // onSelect={onSelect}
                 // onExpand={onExpand}
                 treeData={treeData}
-                />
+                /> */}
+            
+            {treeData ? getCollapseableTree(treeData[0]): undefined}
+            
         </PageContainer>
     );
 };

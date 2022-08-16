a7d720d91e29a362d1d32d6c017201e1403966d7
Markus Schneider <markus@beefcafe.de>
2022-08-16 13:43:32 +0200

    Add basic Playwright e2e test. Configure Jest to ignore e2e tests and Playwright src folder.

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -5,8 +5,12 @@ import { devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
+  testDir: 'src/e2e',
   use: {
+    baseURL: 'https://localhost',
     trace: 'on-first-retry',
+    ignoreHTTPSErrors: true,
+    screenshot: 'only-on-failure',
   },
   projects: [
     {

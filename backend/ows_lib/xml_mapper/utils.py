49d2af44233806cbc455c63ebd8b0b544c55c580
jokiefer <jonas.kiefer@live.com>
2022-08-23 16:58:40 +0200

    Update utils.py

--- backend/ows_lib/xml_mapper/utils.py
+++ backend/ows_lib/xml_mapper/utils.py
@@ -2,3 +2,8 @@ def remove_from_list_by_name(list, name):
     obj_to_be_remove = next((obj for obj in list if obj == name), None)
     if obj_to_be_remove:
         list.remove(list.index(obj_to_be_remove))
+
+
+def get_xml_mapper(capabilities_xml):
+    # TODO: get the corret xml mapper based on the information found inside the given capabilities xml
+    pass

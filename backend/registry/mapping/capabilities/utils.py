cc7818a9d87025d2704c5013b0b02a5be5b0b399
jokiefer <jonas.kiefer@live.com>
2023-02-08 12:36:05 +0100

    Update utils.py

--- backend/registry/mapping/capabilities/utils.py
+++ backend/registry/mapping/capabilities/utils.py
@@ -12,13 +12,20 @@ def get_import_path_for_xml_mapper(service):
 
 
 def get_mapper_for_service(service):
-    from registry.models.service import WebMapService
+    from registry.models.service import WebFeatureService, WebMapService
 
     if isinstance(service, WebMapService):
-        if service.version == "1.3.0":
+        from backend.registry.mapping.capabilities.wms.wms import \
+            WebMapServiceToXml
+        if service.version == "1.1.1":
+            from ows_lib.xml_mapper.capabilities.wms.wms111 import \
+                WebMapService as XmlWebMapService
+        elif service.version == "1.3.0":
             from ows_lib.xml_mapper.capabilities.wms.wms130 import \
                 WebMapService as XmlWebMapService
-
-            from backend.registry.mapping.capabilities.wms.wms import \
-                WebMapServiceToXml
-            return forward_mapping_factory(from_obj=WebMapService, to_obj=XmlWebMapService, base_mapping=WebMapServiceToXml, mappings=[assign(to_field="keywords", action=WebMapServiceToXml.keywords, to_list=True)])
+        else:
+            raise NotImplementedError(
+                f"wms of version: {service.version} is not supported.")
+        return forward_mapping_factory(from_obj=WebMapService, to_obj=XmlWebMapService, base_mapping=WebMapServiceToXml, mappings=[assign(to_field="keywords", action=WebMapServiceToXml.keywords, to_list=True)])
+    elif isinstance(service, WebFeatureService):
+        raise NotImplementedError("TODO")

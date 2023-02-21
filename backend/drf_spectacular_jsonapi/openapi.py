3990af4f5de407b224545cbd6e1928bbf42727dc
jokiefer <jonas.kiefer@live.com>
2023-02-21 12:34:37 +0100

    Update openapi.py

--- backend/drf_spectacular_jsonapi/openapi.py
+++ backend/drf_spectacular_jsonapi/openapi.py
@@ -1,7 +1,8 @@
+import typing
 import warnings
 from datetime import datetime
 from inspect import signature
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 from django.conf import settings
 from django.urls import resolve
@@ -12,6 +13,7 @@ from rest_framework import serializers as drf_serializers
 from rest_framework.exceptions import APIException
 from rest_framework.fields import empty
 from rest_framework.schemas.utils import is_list_view
+from rest_framework.serializers import Serializer
 from rest_framework_gis.fields import GeometryField
 from rest_framework_json_api import serializers, views
 from rest_framework_json_api.utils import (format_field_name,
@@ -25,7 +27,7 @@ class JsonApiAutoSchema(AutoSchema):
     Extend DRF's openapi.AutoSchema for JSON:API serialization.
     """
 
-    def get_serializer(self, path, method):
+    def get_serializer(self, path, method) -> Serializer | None:
         view = self.view
 
         if not hasattr(view, 'get_serializer'):
@@ -40,13 +42,17 @@ class JsonApiAutoSchema(AutoSchema):
                           .format(view.__class__.__name__, method, path))
             return None
 
-    def _get_filter_parameters(self):
+    def _get_serializer_field(self, name: str) -> Dict | None:
+        """Retuns serializer field object by the name of the field"""
+        serializer = self.get_serializer(path=self.path, method=self.method)
+        return serializer.fields.get(name, None)
+
+    def _get_filter_parameters(self) -> Dict:
         """ JSON:API specific handling for sort parameter
 
         See also json:api docs: https://jsonapi.org/format/#fetching-sorting
         """
         parameters = super()._get_filter_parameters()
-
         sort_param = next(
             (parameter for parameter in parameters if parameter["name"] == "sort"), None)
         if sort_param and hasattr(self.view, "ordering_fields") and self.view.ordering_fields:
@@ -57,13 +63,58 @@ class JsonApiAutoSchema(AutoSchema):
 
             # So for that case, sorting is not supported for this endpoint. We need to drop the sort filter parameter.
             parameters.pop(parameters.index(sort_param))
+
+        self._patch_translations_for_fields(parameters=parameters)
+
         return parameters
 
-    def _patch_description_for_field(self, parameters):
-        serializer = self.get_serializer(self.path, self.method)
+    def get_label_for_filter(self, filter_obj):
+        # explcit title defined by user
+        # cast to string to ensure django gettext_lazy is rendered correctly
+        return str(filter_obj.label) if filter_obj.label else None
+
+    def get_help_text_for_filter(self, filter_obj):
+        # explicit help_text defined by user
+        # cast to string to ensure django gettext_lazy is rendered correctly
+        return str(filter_obj.help_text) if hasattr(filter_obj, "help_text") and filter_obj.help_text else None
+
+    def get_title_and_description_for_filter_parameter(self, field_name) -> Tuple[str, str]:
+        title = None
+        description = None
+        if hasattr(self.view, "filterset_class"):
+            # only in case of filterset classes there is a possibility,
+            # that the user set the label or title expliciet to describe what this filter does.
+            _filter = self.view.filterset_class.declared_filters.get(
+                field_name, None)
+            if _filter:
+                # only lookup into user specified filters,
+                # cause otherwise the title and label are made from the django model stack.
+                title = self.get_label_for_filter(filter_obj=_filter)
+                description = self.get_help_text_for_filter(
+                    filter_obj=_filter)
+
+        return title, description
+
+    def _get_field_name_from_filter_parameter_name(self, parameter_name):
+        field_name = parameter_name.split("[")[1].split("]")[0]
+        if "_" in field_name:
+            field_name = field_name.split("_")[0]
+        if "." in field_name:
+            field_name = field_name.split(".")[0]
+        return field_name
+
+    def _patch_translations_for_fields(self, parameters: Dict):
+        """Patching all parameter descriptions with the django translations"""
         for parameter in parameters:
-            next(field for field in serializer.fields.values() if field.name)
-        pass
+            if "filter[" in parameter.get("name", ""):
+                field_name = self._get_field_name_from_filter_parameter_name(
+                    parameter_name=parameter["name"])
+                title, description = self.get_title_and_description_for_filter_parameter(
+                    field_name=field_name)
+                if title:
+                    parameter["title"] = title
+                if description:
+                    parameter["description"] = description
 
     def _patch_sort_param_schema(self, sort_param: Dict) -> None:
         """Patching all possible sortable columns as schema definition."""

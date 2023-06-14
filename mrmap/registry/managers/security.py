d31846eb825069be2e74fdfcafe6e1f09076279e
jokiefer <jonas.kiefer@live.com>
2023-06-14 10:39:53 +0200

    Update security.py

--- mrmap/registry/managers/security.py
+++ mrmap/registry/managers/security.py
@@ -1,5 +1,5 @@
 from abc import ABC
-from typing import Any, List
+from typing import Any, List, Tuple
 
 from django.contrib.auth.models import Group
 from django.contrib.gis.db.models import Union
@@ -20,7 +20,7 @@ from registry.settings import SECURE_ABLE_OPERATIONS_LOWER
 
 class AllowedOgcServiceOperationQuerySet(ABC, models.QuerySet):
 
-    def get_entity_identifiers(self, request) -> tuple[str, List[str]]:
+    def get_entity_identifiers(self, request) -> Tuple[str, List[str]]:
         raise NotImplementedError
 
     def filter_by_requested_entity(self, request):

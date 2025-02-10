3c861c99d0ab454008ccce1f64757227aa16c865
jokiefer <jonas.kiefer@live.com>
2025-02-10 15:05:58 +0100

    adds jsonapi based referencefield

---
+++ frontend/src/jsonapi/components/ReferenceField.tsx
@@ -0,0 +1,29 @@
+import { Link, RaRecord, ReferenceField, ReferenceFieldProps, useFieldValue, useGetRecordRepresentation } from "react-admin";
+import { hasIncludedData } from "../utils";
+
+const JsonApiReferenceField = <
+    RecordType extends Record<string, any> = Record<string, any>,
+    ReferenceRecordType extends RaRecord = RaRecord,
+>(
+    props: ReferenceFieldProps<RecordType, ReferenceRecordType>
+) => {
+    const data = useFieldValue(props);
+    const { source, emptyText, reference } = props;
+    const getRecordRepresentation = useGetRecordRepresentation(reference);
+
+    if (typeof data === 'object' && !Array.isArray(data) && data !== null && hasIncludedData(data) ) {
+      return (
+        <Link to={`/${reference}/${data.id}/show`} >
+          {getRecordRepresentation(data)}
+        </Link>
+      )
+    }
+    console.log('hoho', data)
+
+    return (
+      <ReferenceField {...props} source={`${source}.id`} />
+    )
+    
+};
+
+export default JsonApiReferenceField;

72c741741eed9df66eec9ef431dc8f61d1d79667
jokiefer <jonas.kiefer@live.com>
2025-07-30 16:23:39 +0200

    Update base.txt, urls.py, and 2 more files...

--- backend/notify/urls.py
+++ backend/notify/urls.py
@@ -4,18 +4,19 @@ from rest_framework_extensions.routers import ExtendedSimpleRouter
 app_name = 'notify'
 
 router = ExtendedSimpleRouter(trailing_slash=False)
+background_process_routes = router.register(
+    r'background-processes', views.BackgroundProcessViewSet, basename='backgroundprocess')
+background_process_routes.register(r'logs', views.NestedBackgroundProcessLogViewSet,
+                                   basename='backgroundprocess-logs', parents_query_lookups=['background_process'])
+background_process_routes.register(r'threads', views.NestedTaskResultReadOnlyViewSet,
+                                   basename='backgroundprocess-threads', parents_query_lookups=['process'])
 (
     # jobs
     router.register(r'task-results',
                     views.TaskResultReadOnlyViewSet, basename='taskresult'),
 
     # background prcesses
-    router.register(r'background-processes',
-                    views.BackgroundProcessViewSet, basename='backgroundprocess')
-    .register(r'logs', views.NestedBackgroundProcessLogViewSet, basename='backgroundprocess-logs', parents_query_lookups=['background_process']),
-    router.register(r'background-processes',
-                    views.BackgroundProcessViewSet, basename='backgroundprocess')
-    .register(r'threads', views.NestedTaskResultReadOnlyViewSet, basename='backgroundprocess-threads', parents_query_lookups=['process']),
+    background_process_routes,
 
     router.register(r'background-processes-log',
                     views.BackgroundProcessLogViewSet, basename='backgroundprocesslog'),

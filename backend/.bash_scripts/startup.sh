932433356e006b0cfaa6f3f64929a5915d8764c4
jokiefer <jonas.kiefer@live.com>
2025-09-03 11:59:30 +0200

    Update startup.sh and docker-compose.yml

--- backend/.bash_scripts/startup.sh
+++ backend/.bash_scripts/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 source /opt/venv/bin/activate
-gunicorn -b 0.0.0.0:8001 -k uvicorn.workers.UvicornWorker --workers=4 --log-level=info --timeout=0 MrMap.asgi:application
+gunicorn -b 0.0.0.0:8001 -k uvicorn.workers.UvicornWorker --workers=$GUNICORN_WORKERS --log-level=info --timeout=0 MrMap.asgi:application

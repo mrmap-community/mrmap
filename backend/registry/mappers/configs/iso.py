abbf7f2ed9abcb280e86ac7a49e9ac10807c8c2a
jokiefer <jonas.kiefer@live.com>
2025-11-28 11:33:16 +0100

    work on iso parser config

--- backend/registry/mappers/configs/iso.py
+++ backend/registry/mappers/configs/iso.py
@@ -15,19 +15,38 @@ XPATH_MAP = {
             "_model": "registry.DatasetMetadataRecord",
             "_base_xpath": "/gmd:MD_Metadata",
             "fields": {
-                "title": "",
-                "abstract": "",
+                "title": "./gmd:citation/gmd:CI_Citation/gmd:title/gco:CharacterString",
+                "abstract": "./gmd:abstract/gco:CharacterString",
+                "access_constraints": "./gmd:resourceConstraints/gmd:MD_LegalConstraints[gmd:accessConstraints/gmd:MD_RestrictionCode/@codeListValue=\"otherRestrictions\"]/gmd:otherConstraints/gco:CharacterString",
                 "code": "",
                 "code_space": "",
-                "date_stamp": "",
-
+                # TODO: maybe parser is needed to convert simple Date information to DateTime
+                "date_stamp": "./gmd:dateStamp/*[self::gco:DateTime or self::gco:Date]",
                 "bounding_geometry": "",
                 "spatial_res_type": "",
                 "spatial_res_value": "",
+                "file_identifier": "./gmd:fileIdentifier/gco:CharacterString",
                 "keywords": {
+                    "_model": "registry.Keyword",
+                    "_base_xpath": "./gmd:descriptiveKeywords/gmd:MD_Keywords/gmd:keyword",
+                    "_create_mode": "get_or_create",
+                    "_many": True,
+                    "fields": {
+                      "keyword": "./gco:CharacterString"
+                    }
 
                 },
-                "file_identifier": "gmd:fileIdentifier/gco:CharacterString",
+                "dataset_contact": {
+                    "_model": "registry.MetadataContact",
+                    "_base_xpath": "./gmd:pointOfContact/gmd:CI_ResponsibleParty",
+                    "_create_mode": "get_or_create",
+                    "fields": {
+                        "name": "./gmd:organisationName/gco:CharacterString",
+                        "person_name": "./gmd:individualName/gco:CharacterString",
+                        "phone": "./gmd:contactInfo/gmd:CI_Contact/gmd:phone/gmd:CI_Telephone/gmd:voice/gco:CharacterString",
+                        "email": "./gmd:contactInfo/gmd:CI_Contact/gmd:address/gmd:CI_Address/gmd:electronicMailAddress/gco:CharacterString",
+                    }
+                },
                 "metadata_contact": {
                     "_model": "registry.MetadataContact",
                     "_base_xpath": "./gmd:referenceSystemInfo/gmd:MD_ReferenceSystem/gmd:referenceSystemIdentifier/gmd:RS_Identifier",

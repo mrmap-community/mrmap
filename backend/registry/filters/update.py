c87a86f0ebc5f5c93ecd03668fe4f438f4228a05
Ole Harth <o.harth@lvgl.saarland.de>
2026-05-22 10:28:01 +0200

    add FilterSets for WebFeatureServiceUpdateJob and FeatureTypeMapping

--- backend/registry/filters/update.py
+++ backend/registry/filters/update.py
@@ -1,6 +1,11 @@
 from django.utils.translation import gettext_lazy as _
 from django_filters.filterset import FilterSet
-from registry.models.update import LayerMapping, WebMapServiceUpdateJob
+from registry.models.update import (
+    FeatureTypeMapping,
+    LayerMapping,
+    WebFeatureServiceUpdateJob,
+    WebMapServiceUpdateJob,
+)
 
 
 class WebMapServiceUpdateJobFilterSet(FilterSet):
@@ -28,4 +33,29 @@ class LayerMappingFilterSet(FilterSet):
             "old_layer": ['exact', ],
             "created": ['exact', 'icontains', 'contains', 'in'],
             "is_confirmed": ['exact',],
+        }
+
+
+class WebFeatureServiceUpdateJobFilterSet(FilterSet):
+    class Meta:
+        model = WebFeatureServiceUpdateJob
+        fields = {
+            "id": ["exact", "icontains", "contains", "in"],
+            "service": ["exact"],
+            "status": ["exact", "icontains", "contains", "in"],
+            "date_created": ["exact", "icontains", "contains", "in"],
+            "done_at": ["exact", "icontains", "contains", "in"],
+        }
+
+
+class FeatureTypeMappingFilterSet(FilterSet):
+    class Meta:
+        model = FeatureTypeMapping
+        fields = {
+            "id": ["exact", "icontains", "contains", "in"],
+            "job": ["exact"],
+            "new_featuretype": ["exact"],
+            "old_featuretype": ["exact"],
+            "created": ["exact", "icontains", "contains", "in"],
+            "is_confirmed": ["exact"],
         }

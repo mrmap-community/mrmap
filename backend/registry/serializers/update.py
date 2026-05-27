c3fc17d730fedf57c72aab99f67e8dcc7ebb99c6
Ole Harth <o.harth@lvgl.saarland.de>
2026-05-27 10:32:41 +0200

    add service update base serializers
    UpdateJobBaseSerializer and MappingBaseSerializer for WebMapServiceUpdateJobSerializer and LayerMappingSerializer

--- backend/registry/serializers/update.py
+++ backend/registry/serializers/update.py
@@ -3,42 +3,67 @@ from registry.models.service import Layer, WebMapService
 from registry.models.update import LayerMapping, WebMapServiceUpdateJob
 from registry.serializers.service import WebMapServiceSerializer
 from rest_framework_json_api.relations import ResourceRelatedField
-from rest_framework_json_api.serializers import (BooleanField, DateTimeField,
-                                                 HyperlinkedIdentityField,
-                                                 IntegerField, ModelSerializer)
+from rest_framework_json_api.serializers import (
+    BooleanField,
+    DateTimeField,
+    HyperlinkedIdentityField,
+    IntegerField,
+    ModelSerializer,
+    Serializer,
+)
 
 
-class LayerMappingSerializer(ModelSerializer):
-    url = HyperlinkedIdentityField(
-        view_name="registry:layermapping-detail",
-        read_only=True
+class UpdateJobBaseSerializer(Serializer):
+    date_created = DateTimeField(
+        label=_("Created"),
+        help_text=_("The date and time when this update job was created."),
+        read_only=True,
+    )
+    done_at = DateTimeField(
+        label=_("Done"),
+        help_text=_("The date and time when this update job was completed."),
+        read_only=True,
+    )
+    status = IntegerField(
+        label=_("Status"),
+        help_text=_("The current status of the update job."),
+        read_only=True,
     )
 
+
+class MappingBaseSerializer(Serializer):
+    created = DateTimeField(
+        label=_("Created"),
+        help_text=_("The date and time when this layer mapping was created."),
+        read_only=True,
+    )
+    is_confirmed = BooleanField(
+        label=_("Is Confirmed"),
+        help_text=_("Whether this layer mapping is confirmed or not."),
+        default=False,
+    )
+
+
+class LayerMappingSerializer(MappingBaseSerializer, ModelSerializer):
+    url = HyperlinkedIdentityField(
+        view_name="registry:layermapping-detail",
+        read_only=True,
+    )
     job = ResourceRelatedField(
         label=_("Update Job"),
         help_text=_("The update job this layer mapping belongs to."),
-        queryset=WebMapServiceUpdateJob.objects
+        queryset=WebMapServiceUpdateJob.objects,
     )
     new_layer = ResourceRelatedField(
         label=_("New Layer"),
         help_text=_("The new layer this mapping points to."),
-        queryset=Layer.objects
+        queryset=Layer.objects,
     )
     old_layer = ResourceRelatedField(
         label=_("Old Layer"),
         help_text=_("The old layer this mapping points to."),
         queryset=Layer.objects,
-        required=False
-    )
-    created = DateTimeField(
-        label=_("Created"),
-        help_text=_("The date and time when this layer mapping was created."),
-        read_only=True
-    )
-    is_confirmed = BooleanField(
-        label=_("Is Confirmed"),
-        help_text=_("Whether this layer mapping is confirmed or not."),
-        default=False
+        required=False,
     )
 
     included_serializers = {
@@ -51,38 +76,22 @@ class LayerMappingSerializer(ModelSerializer):
                   "new_layer", "created", "is_confirmed")
 
 
-class WebMapServiceUpdateJobSerializer(ModelSerializer):
+class WebMapServiceUpdateJobSerializer(UpdateJobBaseSerializer, ModelSerializer):
     url = HyperlinkedIdentityField(
         view_name="registry:webmapserviceupdatejob-detail",
-        read_only=True
+        read_only=True,
     )
     service = ResourceRelatedField(
         label=_("Web Map Service"),
         help_text=_("The web map service this update job belongs to."),
-        queryset=WebMapService.objects
+        queryset=WebMapService.objects,
     )
     update_candidate = ResourceRelatedField(
         source="service.webmapservice_update_candidate",
         label=_("Update Candidate"),
-        help_text=_(
-            "The web map service this update job is a candidate for updating."),
+        help_text=_("The web map service this update job is a candidate for updating."),
         model=WebMapService,
-        read_only=True
-    )
-    date_created = DateTimeField(
-        label=_("Created"),
-        help_text=_("The date and time when this update job was created."),
-        read_only=True
-    )
-    done_at = DateTimeField(
-        label=_("Done"),
-        help_text=_("The date and time when this update job was completed."),
-        read_only=True
-    )
-    status = IntegerField(
-        label=_("Status"),
-        help_text=_("The current status of the update job."),
-        read_only=True
+        read_only=True,
     )
     mappings = ResourceRelatedField(
         model=LayerMapping,
